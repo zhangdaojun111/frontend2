1b000b438622d05fec10694a75de0ce04ea37ac0
xiongxiaotao <xxtao110@163.com>
2017-07-24 10:50:34 +0800

    http的API设计

---
+++ app/lib/http.js
@@ -0,0 +1,38 @@
+export default {
+
+
+    /**
+     * 请求不回立即发送，会被缓存起来，直到调用了flush时才会和其他缓存的请求一起发送
+     *
+     */
+    get: function() {
+
+    },
+
+    post: function() {
+
+    },
+
+    
+    flush: function() {
+        
+    },
+
+    /**
+     *  直接发送请求
+     */
+    getImmediately: function() {
+
+    },
+
+    postImmediately: function() {
+
+    },
+
+
+    ajax: function() {
+
+    }
+
+
+}
