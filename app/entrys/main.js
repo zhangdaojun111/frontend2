e07b0845de051f7965801d686ffd34d4cafc8624
XiongXiaotao <xxtao110@163.com>
2017-07-26 18:50:41 +0800

    新建分支

--- app/entrys/main.js
+++ app/entrys/main.js
@@ -5,12 +5,30 @@ import 'jquery-ui/ui/widgets/button.js';
 import 'jquery-ui/ui/widgets/dialog.js';
 
 import Login from '../components/login/login';
+import http from '../lib/http';
 
 $('#login').button({
     label: '点击登录'
 }).on('click', function() {
     Login.show();
 });
+
+async function wait() {
+    let data = await $.ajax({
+        async:false,
+        url: 'https://api.asilu.com/weather/',
+        type: "GET",
+        dataType: 'jsonp',
+        jsonp: 'callback',
+        data: {
+            city: '济宁'
+        },
+        timeout: 5000
+    });
+    console.log(data);
+    console.log('hello world 123123');
+}
+wait();
 
 $('#active').button().on('click', function() {
 
--- app/lib/http.js
+++ app/lib/http.js
@@ -1,40 +1,58 @@
 
+$.ajaxSetup({
+    dataType: 'json'
+});
+
+const GetSet = new Set();
+const PostSet = new Set();
+const Hash = {};
+const prefix = 'erds-';
+let counter = 10000;
+
+
+
+
+function getKey() {
+    return prefix + counter ++;
+}
 
 
 export default {
 
     /**
-     * 请求不回立即发送，会被缓存起来，直到调用了flush时才会和其他缓存的请求一起发送
-     *
+     * 缓存的get请求，必须通过flush才能发送
+     * @param url
+     * @param params
+     * @returns {Promise}
      */
-    get: function() {
-
+    get: function(url, params) {
+        const key = getKey();
+        GetSet.add({
+            url: url,
+            key: key,
+            params: params
+        });
+        const promise = new Promise((resolve) => {
+            Hash[key] = resolve;
+        });
+        return promise;
     },
 
-    post: function() {
-
+    post: function(url, data) {
+        const key = getKey();
+        PostSet.add({
+            url: url,
+            key: key,
+            params: params
+        });
+        const promise = new Promise((resolve) => {
+            Hash[key] = resolve;
+        });
+        return promise;
     },
 
-    
     flush: function() {
         
-    },
-
-    /**
-     *  直接发送请求
-     */
-    getImmediately: function() {
-
-    },
-
-    postImmediately: function() {
-
-    },
-
-
-    ajax: function() {
-
     }
-
 
 }
