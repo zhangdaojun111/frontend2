a2120ff6642bbdbc4713e81d79dd733fd552aaec
luoweiqiang <15681276901@163.com>
2017-07-31 21:07:41 +0800

    BI左侧导航修改

--- app/components/bisystem/aside/bisystem.nav.js
+++ app/components/bisystem/aside/bisystem.nav.js
@@ -1,20 +1,54 @@
+import {BiBaseComponent} from '../bi.base.component';
 
-//显示右侧编辑删除按钮
-$('.charts-items li').each(function (index) {
-    $(this).hover(
-        function () {
-            $('.btn_ripple').eq(index).show();
-        },
-        function () {
-            $('.btn_ripple').eq(index).hide();
-        }
-    )
-});
+import template from './bisystem.nav.html';
+import './bisystem.nav.scss';
 
-//点击出现弹出框
-$('.btn_ripple').each(function (index) {
-    $(this).click(function () {
-        // console.log(index);
+let AsideConfig = {
+    template: template,
 
-    })
-})
+    actions:{
+
+    },
+
+    afterRender:function () {
+
+        // 图标/图标 阴影效果切换
+        $('.user a').each(function () {
+            // console.log($(this));
+            $(this).on('click',function () {
+                $(this).addClass('active');
+                $(this).siblings().removeClass('active');
+            })
+        });
+
+        //显示右侧编辑删除按钮
+        $('.charts-items li').each(function (index) {
+            $(this).hover(
+                function () {
+                    $('.btn_ripple').eq(index).show();
+                },
+                function () {
+                    $('.btn_ripple').eq(index).hide();
+                }
+            )
+        });
+
+        //点击显示隐藏菜单
+        $('.btn_ripple').each(function () {
+            $(this).on('click',function () {
+                $('.hide_meun').eq(0).fadeIn('normal');
+                $('body').mousedown(function () {
+                    $('.hide_meun').eq(0).hide();
+                })
+            })
+        });
+    }
+};
+
+class AsideNav extends BiBaseComponent{
+    constructor() {
+        super(AsideConfig)
+    }
+}
+
+export default AsideNav;
