7745937719a7be91f36bb29f3233d18112b9f2f6
XiongXiaotao <xxtao110@163.com>
2017-08-01 15:34:18 +0800

    获取管理员数据

--- app/entrys/main.js
+++ app/entrys/main.js
@@ -4,18 +4,16 @@ import '../assets/scss/main.scss';
 // import 'jquery-ui/ui/widgets/button.js';
 import 'jquery-ui/ui/widgets/dialog.js';
 
-import {MenuComponent} from '../components/main/menu-full/menu.full';
 import {IframeInstance} from '../components/main/iframes/iframes';
 import {HeaderInstance} from '../components/main/header/header';
 import {AsideInstance} from '../components/main/aside/aside';
-
 
 _.defaultsDeep(AsideInstance.data, {
     systemName: window.config.sysConfig.logic_config.sap_login_system_name,
     avatar: window.config.sysConfig.userInfo.avatar,
     username: window.config.sysConfig.userInfo.username,
-    role: window.config.sysConfig.userInfo.name,
-})
+    role: window.config.sysConfig.userInfo.is_superuser === 1 ? '管理员' : '普通用户'
+});
 AsideInstance.render($('#aside'));
 IframeInstance.render($('#content'));
 HeaderInstance.render($('#header'));
