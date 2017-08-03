fbfbfebb90132a25066f22e7fa01e5b15c4fc39a
yudeping <yudeping@wanwujieshu.net>
2017-08-03 20:36:15 +0800

    []

--- app/services/formService/formService.js
+++ app/services/formService/formService.js
@@ -4,19 +4,19 @@ export const FormService={
     getCountData:async function(json){
         console.log(json);
         let data=this.formatParams(json);
-        return await HTTP.postImmediately({url:'http://127.0.0.1:8081/get_count_data/',data:data});
+        return await HTTP.postImmediately({url:'http://192.168.2.223:9001/get_count_data/',data:data});
     },
     get_exp_value:async function(eval_exps){
         let data=this.formatParams( {"eval_exps": eval_exps} );
-        return await HTTP.postImmediately({url:'http://127.0.0.1:8081/eval_exp_fun/',data:data});
+        return await HTTP.postImmediately({url:'http://192.168.2.223:9001/eval_exp_fun/',data:data});
     },
     getDefaultValue:async function(json){
         let data=this.formatParams(json);
-        return await HTTP.postImmediately({url:'http://127.0.0.1:8081/get_workflow_default_values/',data:data});
+        return await HTTP.postImmediately({url:'http://192.168.2.223:9001/get_workflow_default_values/',data:data});
     },
     getAboutData:async function(json){
         let data=this.formatParams(json);
-        return await HTTP.postImmediately({url:'http://127.0.0.1:8081/get_about_data/',data:data});
+        return await HTTP.postImmediately({url:'http://192.168.2.223:9001/get_about_data/',data:data});
     },
     formatParams(params) {
         let result = [];
@@ -31,7 +31,7 @@ export const FormService={
     },
     getDynamicData:async function({tableId,real_id,seqid}){
         return await HTTP.postImmediately({
-            url: `http://127.0.0.1:8081/get_form_dynamic_data/?seqid=${seqid}&table_id=${tableId}&is_extra=&form_id=`,
+            url: `http://192.168.2.223:9001/get_form_dynamic_data/?seqid=${seqid}&table_id=${tableId}&is_extra=&form_id=`,
             type: "POST",
             data: {
                 form_id:'',
