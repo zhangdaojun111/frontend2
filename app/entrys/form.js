023f2a27b036f3aaa2081867416d18b8e629d95c
yudeping <yudeping@wanwujieshu.net>
2017-08-03 18:19:36 +0800

    [feature]提供form入口

--- app/entrys/form.js
+++ app/entrys/form.js
@@ -2,9 +2,23 @@ import FormBase from '../components/form/base-form/base-form'
 import {HTTP} from '../lib/http';
 import '../components/form/vender/my-multiSelect/my-multiSelect'
 import '../components/form/vender/my-multiSelect/my-multiSelect.css'
-
-let formBase;
-function hasKeyInFormDataStatic(key,staticData){
+let FormEntrys={
+    formBase:null,
+    init:function(config={}){
+        this.tableId=config.tableId||'';
+        this.parentRealId=config.parentRealId||'';
+        this.parentTempId=config.parentTempId||'';
+        this.seqId=config.seqId||'';
+        this.realId=config.realId||'';
+        this.parentTableId=config.parentTableId||'';
+        this.parentRecordId=config.parentRecordId||'';
+        this.isView=config.isView||0;
+        this.isBatch=config.isBatch||0;
+        this.recordId=config.recordId||'';
+        this.action=config.action||'';
+        this.el=config.el||'';
+    },
+    hasKeyInFormDataStatic:function (key,staticData){
     let isExist = false;
     for(let dict of staticData["data"]){
         if(dict["dfield"] == key){
@@ -12,12 +26,12 @@ function hasKeyInFormDataStatic(key,staticData){
         }
     }
     return isExist;
-}
+},
 //merge静态和动态数据
-function mergeFormData(staticData,dynamicData){
+    mergeFormData:function (staticData,dynamicData){
     //merge数据
     for(let dfield in dynamicData["data"]){
-        if(hasKeyInFormDataStatic(dfield,staticData)){
+        if(this.hasKeyInFormDataStatic(dfield,staticData)){
             for(let dict of staticData["data"]){
                 if(dict["dfield"] == dfield){
                     for(let k in dynamicData["data"][dfield]){
@@ -36,15 +50,15 @@ function mergeFormData(staticData,dynamicData){
     let data={
 
     }
-    parseRes(staticData);
+    this.parseRes(staticData);
     for(let obj of staticData.data){
         data[obj.dfield]=obj;
     }
     staticData.data=data;
     return staticData;
-}
-
-function parseRes(res){
+},
+//处理数据
+    parseRes:function (res){
     if(res !== null){
         let formData = res["data"];
         if(formData.length != 0){
@@ -89,99 +103,38 @@ function parseRes(res){
             }
         }
     }
-}
-
-$('#toEdit').on('click',function(){
-    if(formBase){
-        formBase.destroySelf();
-    }
-    let el=$('<div style="border: 1px solid red;background:#fff;position: fixed;width: 100%;height:100%;overflow: auto">').appendTo('body');
-    let template=`<div class="form">
-                  <div data-dfield="f23" data-type="Textarea" data-width="500"/>
-                  <div data-dfield="f8" data-type="Radio" data-width="300"/>
-                  <div data-dfield="f26" data-type="Input" data-width="300"/>
-                  <div data-dfield="f31" data-type="Input" data-width="300"/>
-                  <div data-dfield="f7" data-type="Select" data-width="300"/>
-                  <div data-dfield="f6" data-type="Year" data-width="300"/>
-                  <div data-dfield="f5" data-type="Buildin" data-width="300"/>
-                  <div data-dfield="f10" data-type="MultiLinkage" data-width="300"/>
-                   <div data-dfield="f24" data-type="Input" data-width="300"/>
-                   <div data-dfield="f30" data-type="Input" data-width="300"/>
-                   <div data-dfield="f14" data-type="Input" data-width="300"/>
-                   <div data-dfield="f29" data-type="Input" data-width="300"/>
-                   <div data-dfield="f25" data-type="Input" data-width="300"/>
-                   <div data-dfield="f15" data-type="Input" data-width="300"/>
-                  <div data-dfield="f11" data-type="Readonly" data-width="300"/>
-                   <div data-dfield="f233" data-type="Hidden" data-width="300"/>
-                  <div data-dfield="f27" data-type="Password" data-width="300"/>
-                  <div data-dfield="f28" data-type="YearMonthControl" data-width="300"/>
-                  </div>
-                    `;
-    let real_id=$('#real_id').get(0).value||'';
-    let is_view=$('#is_view').get(0).value||0;
-    wait(el,template,'yudeping','8696_yz7BRBJPyWnbud4s6ckU7e',real_id,is_view);
-});
-$('#count').on('click',function(){
-    if(formBase){
-        formBase.destroySelf();
-    }
-    let el=$('<div style="border: 1px solid red;background:#fff;position: fixed;width: 100%;height:100%;overflow: auto">').appendTo('body');
-    let template=`<div class="form">
-              <div data-dfield="f5" data-type="Input" data-width="300"/>
-              <div data-dfield="f6" data-type="Readonly" data-width="300"/>
-              </div>
-                `;
-    let real_id=$('#real_id').get(0).value||'';
-    let is_view=$('#is_view').get(0).value||0;
-    wait(el,template,'yudeping','7051_UoWnaxPaVSZhZcxZPbEDpG',real_id,is_view);
-});
-$('#editRequired').on('click',function(){
-    if(formBase){
-        formBase.destroySelf();
-    }
-    let el=$('<div style="border: 1px solid red;background:#fff;position: fixed;width: 100%;height:100%;overflow: auto">').appendTo('body');
-    let template=`<div class="form">
-              <div data-dfield="f5" data-type="Input" data-width="300"/>
-              <div data-dfield="f6" data-type="Radio" data-width="300"/>
-              </div>
-                `;
-    let real_id=$('#real_id').get(0).value||'';
-    let is_view=$('#is_view').get(0).value||0;
-    wait(el,template,'yudeping','3461_P28RYPGTGGE7DVXH8LBMHe',real_id,is_view);
-});
-$('#defaultValue').on('click',function(){
-    if(formBase){
-        formBase.destroySelf();
-    }
-    let el=$('<div style="border: 1px solid red;background:#fff;position: fixed;width: 100%;height:100%;overflow: auto">').appendTo('body');
-    //默认值
-    let template=`<div class="form">
-              <div data-dfield="f5" data-type="Input" data-width="300"/>
-              <div data-dfield="f6" data-type="Input" data-width="300"/>
-              </div>
-                `;
-
-    let real_id=$('#real_id').get(0).value||'';
-    let is_view=$('#is_view').get(0).value||0;
-    wait(el,template,'yudeping','1160_ex7EbDsyoexufF2UbXBmSJ',real_id,is_view);
-});
-
-$('#exp').on('click',function(){
-    if(formBase){
-        formBase.destroySelf();
-    }
-    let el=$('<div style="border: 1px solid red;background:#fff;position: fixed;width: 100%;height:100%;overflow: auto">').appendTo('body');
-    let real_id=$('#real_id').get(0).value||'';
-    let is_view=$('#is_view').get(0).value||0;
-    let template='11';
-    wait(el,template,'yudeping','7336_HkkDT7bQQfqBag4kTiFWoa',real_id,is_view);
-})
-
-async function wait(el,template,seqid,table_id,real_id,is_view) {
-    let staticData = await HTTP.postImmediately({
-        url: `/get_form_static_data/?seqid=${seqid}&table_id=${table_id}&is_extra=&form_id=`,
-        type: "POST",
-        data: {
+},
+//请求数据
+    getFormData:async function (el,template,seqid,table_id,real_id,is_view) {
+        // let staticData = await HTTP.postImmediately({
+        //     url: `/get_form_static_data/?seqid=${seqid}&table_id=${table_id}&is_extra=&form_id=`,
+        //     type: "POST",
+        //     data: {
+        //         form_id:'',
+        //         table_id:table_id,
+        //         is_view:is_view,
+        //         parent_table_id:'',
+        //         parent_real_id:'',
+        //         real_id:real_id,
+        //         parent_temp_id:'',
+        //     }
+        // });
+        // let dynamicData = await HTTP.postImmediately({
+        //     url: `/get_form_dynamic_data/?seqid=${seqid}&table_id=${table_id}&is_extra=&form_id=`,
+        //     type: "POST",
+        //     hearder:'',
+        //     data: {
+        //         form_id:'',
+        //         table_id:table_id,
+        //         is_view:is_view,
+        //         parent_table_id:'',
+        //         parent_real_id:'',
+        //         parent_temp_id:'',
+        //         real_id:real_id,
+        //     }
+        // });
+        let _this=this;
+        Promise.all([HTTP.post('get_form_dynamic_data',{
             form_id:'',
             table_id:table_id,
             is_view:is_view,
@@ -189,37 +142,101 @@ async function wait(el,template,seqid,table_id,real_id,is_view) {
             parent_real_id:'',
             real_id:real_id,
             parent_temp_id:'',
-        }
-    });
-    let dynamicData = await HTTP.postImmediately({
-        url: `/get_form_dynamic_data/?seqid=${seqid}&table_id=${table_id}&is_extra=&form_id=`,
-        type: "POST",
-        hearder:'',
-        data: {
+        }),HTTP.post('get_form_static_data',{
             form_id:'',
             table_id:table_id,
             is_view:is_view,
             parent_table_id:'',
             parent_real_id:'',
-            parent_temp_id:'',
             real_id:real_id,
-        }
-    });
-    template=formDefaultVersion(staticData.data);
-    let data=mergeFormData(staticData,dynamicData);
-    let formData={
-        template:template,
-        data:data,
-    }
-    formBase=new FormBase(formData);
-    formBase.render(el);
-}
+            parent_temp_id:'',
+        })]).then(res=>{
+            template=_this.formDefaultVersion(res[1].data);
+            let data=_this.mergeFormData(res[1],res[0]);
+            let formData={
+                template:template,
+                data:data,
+            }
+            _this.formBase=new FormBase(formData);
+            _this.formBase.render(el);
+        })
+        HTTP.flush();
+    },
+    //生成默认表单
 
-function formDefaultVersion(data){
+    formDefaultVersion : function (data){
     let html='<div class="form">';
     for(let obj of data){
         html+=`<div data-dfield="${obj.dfield}" data-type="${obj.type}"></div>`;
     }
     html+='</div>'
     return html;
+},
+
+    createForm:function(config={}){
+        this.init(config);
+        if(this.formBase){
+            this.formBase.destroySelf();
+        }
+        let html=$('<div style="border: 1px solid red;background:#fff;position: fixed;width: 100%;height:100%;overflow: auto">').appendTo(this.el);
+        let template='';
+        this.getFormData(html,template,this.seqId,this.tableId,this.realId,this.isView);
+    }
 }
+
+$('#toEdit').on('click',function(){
+    let real_id=$('#real_id').get(0).value||'';
+    let is_view=$('#is_view').get(0).value||0;
+    FormEntrys.createForm({
+        tableId:'8696_yz7BRBJPyWnbud4s6ckU7e',
+        seqId:'yudeping',
+        el:$('body'),
+        is_view:is_view,
+        real_id:real_id
+    });
+});
+$('#count').on('click',function(){
+    let real_id=$('#real_id').get(0).value||'';
+    let is_view=$('#is_view').get(0).value||0;
+    FormEntrys.createForm({
+        tableId:'7051_UoWnaxPaVSZhZcxZPbEDpG',
+        seqId:'yudeping',
+        el:$('body'),
+        is_view:is_view,
+        real_id:real_id
+    });
+});
+$('#editRequired').on('click',function(){
+    let real_id=$('#real_id').get(0).value||'';
+    let is_view=$('#is_view').get(0).value||0;
+    FormEntrys.createForm({
+        tableId:'3461_P28RYPGTGGE7DVXH8LBMHe',
+        seqId:'yudeping',
+        el:$('body'),
+        is_view:is_view,
+        real_id:real_id
+    });
+});
+$('#defaultValue').on('click',function(){
+    let real_id=$('#real_id').get(0).value||'';
+    let is_view=$('#is_view').get(0).value||0;
+    FormEntrys.createForm({
+        tableId:'1160_ex7EbDsyoexufF2UbXBmSJ',
+        seqId:'yudeping',
+        el:$('body'),
+        is_view:is_view,
+        real_id:real_id
+    });
+});
+$('#exp').on('click',function(){
+    let real_id=$('#real_id').get(0).value||'';
+    let is_view=$('#is_view').get(0).value||0;
+    FormEntrys.createForm({
+        tableId:'7336_HkkDT7bQQfqBag4kTiFWoa',
+        seqId:'yudeping',
+        el:$('body'),
+        is_view:is_view,
+        real_id:real_id
+    });
+})
+export default FormEntrys
--- app/services/formService/formService.js
+++ app/services/formService/formService.js
@@ -44,4 +44,5 @@ export const FormService={
             }
         });
     }
+
 }
