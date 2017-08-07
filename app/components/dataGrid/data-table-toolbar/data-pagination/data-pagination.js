3405884f024169fafe8c9f4108fd608f15457a07
wangkai <631876053@qq.com>
2017-08-07 16:36:37 +0800

    [feature] page-table

--- app/components/dataGrid/data-table-toolbar/data-pagination/data-pagination.js
+++ app/components/dataGrid/data-table-toolbar/data-pagination/data-pagination.js
@@ -22,32 +22,40 @@ let config = {
     actions: {
         //分页数据改变
         paginationChanged: function ( obj ) {//currentPage,rows,firstRow
+        },
+        disableClick:function () {
+            $(this).unbind("click");
+            $(this).css({
+                    "opacity": "0.3",
+                    "filter": "alpha(opacity=30)"
+            });
+            console.log('sss')
         }
     },
     afterRender: function () {
-
         $(".selectPage").hide();
+        this.data.rows = $(".selectSize").val();
+        this.data.sumPage = Math.ceil(this.data.total / this.data.rows);
+        if(this.data.currentPage===1) {
+
+        }
+        else {
+        }
         //监听下拉框的值
         $(".selectSize").change(() => {
             this.data.currentPage = 1;
             this.data.firstRow = 0;
-            this.data.rows = $(".selectSize").val()
-            this.data.sumPage = Math.ceil(this.data.total / this.data.rows);
-            console.log(this.data.sumPage);
             $('.current-page').html(this.data.currentPage);
             $(".sumPage").html("共" + this.data.sumPage + "页");
-
             let obj = {
                 currentPage: this.data.currentPage,
                 rows: this.data.rows,
                 firstRow: this.data.firstRow
-            }
+            };
             this.actions.paginationChanged(obj);
-        }),
+        });
             //点击下一页 当前页面数加1
             $(".goNext").click(() => {
-                this.data.rows = $(".selectSize").val()
-                this.data.sumPage = Math.ceil(this.data.total / this.data.rows);
                 if (this.data.currentPage < this.data.sumPage) {
                     console.log(this.data.currentPage)
                     console.log(this.data.currentPage.type)
@@ -61,16 +69,13 @@ let config = {
                         currentPage: this.data.currentPage,
                         rows: this.data.rows,
                         firstRow: this.data.firstRow
-                    }
+                    };
                     this.actions.paginationChanged(obj);
                 }
 
-            }),
+            });
             //点击上一页 当前页面数减1
             $(".goPre").click(() => {
-                this.data.rows = $(".selectSize").val();
-                console.log(this.data.rows)
-                console.log(this.data)
                 if (this.data.currentPage > 1) {
                     this.data.currentPage -= 1;
                     //this.actions.paginationChanged(this.data.currentPage,this.data.rows,this.data.firstRow);
@@ -83,25 +88,32 @@ let config = {
                     }
                     this.actions.paginationChanged(obj);
                 }
-            })
+            });
         //直接跳到第一页
         $(".goFirst").click(() => {
-            this.data.rows = $(".selectSize").val();
-            this.data.currentPage = 1;
-            this.data.firstRow = 0;
-            $('.current-page').html(this.data.currentPage);
-            let obj = {
-                currentPage: this.data.currentPage,
-                rows: this.data.rows,
-                firstRow: this.data.firstRow
+            //如果页面小于1 禁止点击
+            if (this.data.currentPage===1){
+                this.actions.disableClick();
             }
-            this.actions.paginationChanged(obj);
-
-        })
+               else {
+                this.data.rows = $(".selectSize").val();
+                this.data.currentPage = 1;
+                this.data.firstRow = 0;
+                $('.current-page').html(this.data.currentPage);
+                let obj = {
+                    currentPage: this.data.currentPage,
+                    rows: this.data.rows,
+                    firstRow: this.data.firstRow
+                };
+                this.actions.paginationChanged(obj);
+            }
+        });
         //直接跳到最后一页
         $(".goLast").click(() => {
-            this.data.rows = $(".selectSize").val();
-            this.data.sumPage = Math.ceil(this.data.total / this.data.rows);
+        if (this.data.currentPage===this.data.sumPage){
+             this.actions.disableClick();
+        }
+        else {
             this.data.currentPage = this.data.sumPage;
             this.data.firstRow = (this.data.sumPage - 1) * this.data.rows;
             $('.current-page').html(this.data.currentPage);
@@ -109,26 +121,25 @@ let config = {
                 currentPage: this.data.currentPage,
                 rows: this.data.rows,
                 firstRow: this.data.firstRow
-            }
+            };
             this.actions.paginationChanged(obj);
-
-        }),
+        }
+        });
 
             //点击当前得页码  跳出可选择页码的文本框
             $(".current-page").click(() => {
-                this.data.rows = $(".selectSize").val();
-                this.data.sumPage = Math.ceil(this.data.total / this.data.rows);
                 this.data.firstRow = (this.data.sumPage - 1) * this.data.rows;
                 $(".sumPage").html("共" + this.data.sumPage + "页");
                 $(".selectPage").show();
                 $(".page").hide();
+            });
 
-            })
         //点击确定按钮跳转到目标页面
         $(".confirm").click(() => {
-            this.data.rows = $(".selectSize").val();
             let target = $(".enter-number").val();
-            this.data.sumPage = Math.ceil(this.data.total / this.data.rows);
+            if (target.length===0){
+                alert("输入为空");
+            }
             if (target>this.data.sumPage){
                 alert("最大页面是"+this.data.sumPage+"页")
             }
@@ -142,20 +153,18 @@ let config = {
                     currentPage: this.data.currentPage,
                     rows: this.data.rows,
                     firstRow: this.data.firstRow
-                }
+                };
                 this.actions.paginationChanged(obj);
             }
             console.log(target);
-        }),
+        });
             //点击取消
             $(".cancel").click(() => {
                 $(".selectPage").hide();
                 $(".page").show();
-            })
+            });
         //刷新
         $(".ui-icon-refresh").click(() => {
-            this.data.rows = $(".selectSize").val();
-            this.data.sumPage = Math.ceil(this.data.total / this.data.rows);
             this.data.firstRow =((this.data.currentPage - 1) * this.data.rows);
             this.data.currentPage;
 
