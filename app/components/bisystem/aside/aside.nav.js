c39a666914cb2e5e8ac8c5ba9393299f6f673d47
yincaihua <249410592@qq.com>
2017-08-07 18:01:00 +0800

    解决冲突

--- app/components/bisystem/aside/aside.nav.js
+++ app/components/bisystem/aside/aside.nav.js
@@ -69,25 +69,52 @@ let config = {
 
         //点击 显示隐藏菜单
         $('.btn_ripple').each(function () {
-            $(this).on('click',function () {
+            $(this).on('click',function (event) {
+                let flag = true;
                 let top = $(this).position().top;
-                console.log(top);
                 $('.hide_meun').eq(0).css('top',top);
                 $('.hide_meun').eq(0).fadeIn('normal');
+                event.stopPropagation();
+                //点击消失
+                $(document).bind('click',function () {
+                    if (flag){
+                        $('.hide_meun').eq(0).fadeOut();
+                        flag = false;
+                    }else{
+                        return;
+                    }
+                })
             });
         });
+
+        //点击 跳转页面
         $('.btn_del').click(function () {
-            alert(1);
-            $('.hide_meun').eq(0).hide();
+            confirm('确定删除吗？');
+            $('.hide_meun').eq(0).fadeOut();
         });
         $('.btn_change').click(function () {
-            alert(1);
-            $('.hide_meun').eq(0).hide();
+            alert('这里是跳转路由');
+            $('.hide_meun').eq(0).fadeOut();
         });
 
-        // $('body').click(function () {
-        //     $('.hide_meun').eq(0).hide();
-        // })
+        //模糊搜索
+        $('.filter-match').on('input',function () {
+            //值改变时 隐藏
+            $('.charts-item').css('display','none');
+
+            //填空值时 仍显示
+            if($('.filter-match').val().length<=0){
+                $('.charts-item').css('display','block');
+                return;
+            }
+
+            //模糊匹配 遍历所有 将匹配的显示出来
+            $('.charts-item').each(function (index) {
+                if ($('.charts-item').eq(index).text().substr(0,$('.filter-match').val().length) == $('.filter-match').val()){
+                    $('.charts-item').eq(index).css('display','block');
+                }
+            })
+        })
     }
 };
 
