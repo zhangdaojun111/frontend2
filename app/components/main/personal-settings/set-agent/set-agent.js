6615ef8bd3f7f5c191d093a600e0bab6284c006f
ZY <510980866@qq.com>
2017-08-08 18:39:43 +0800

    write set-agent fun

--- app/components/main/personal-settings/set-agent/set-agent.js
+++ app/components/main/personal-settings/set-agent/set-agent.js
@@ -18,11 +18,9 @@ let config = {
     workflowTree:null,          //工作流数据
     agentList:null,             //代理人数据
 
-    selectWorkflow:null,        //记录被选中的工作流
-    selectAgent:null,           //记录被选中的代理人
+    selectWorkflow:null,        //记录被选中的工作流的id
+    selectedAgent:'',           //记录被选中的代理人
     isOpen:false,               //是否开启代理，默认否
-
-
 
     actions:{
         initData:function () {
@@ -31,6 +29,9 @@ let config = {
                     if(result.success === 1){
                         this.originData = result;
                         this.formatData = [];
+                        this.selectWorkflow = new Set();
+                        this.selectedAgent = '';
+                        this.isOpen = false;
                         $.extend(true,this.formatData,this.originData.data.workflow_list);
                     }else{
                         msgbox.alert("数据加载失败");
@@ -46,9 +47,10 @@ let config = {
         },
         initWorkflow:function () {
             this.actions.formatOriginData(this.formatData);
-            console.log(this.formatData);
             let treeView = new TreeView(this.formatData,{
-                callback:this.actions.selectNode,
+                callback:(event,node) => {
+                    this.actions.selectNode(event,node);
+                },
                 treeType:"MULTI_SELECT",
                 treeName:"workflow-tree"
             });
@@ -58,6 +60,7 @@ let config = {
         formatOriginData:function (nodes) {
             for(let i = 0; i < nodes.length; i++){
                 const node = nodes[i];
+                console.log(node.id);
                 node.text = node.label;
                 node.icon = '';
                 node.selectedIcon = '';
@@ -78,9 +81,6 @@ let config = {
                 }
             }
         },
-        selectNode:function () {
-
-        },
         initAgentList:function () {
             this.agentList = this.originData.data.user_list;
             let $nameList = this.el.find("#name_list");
@@ -90,11 +90,77 @@ let config = {
                 newAgent.html(agent.name);
                 $nameList.append(newAgent);
             }
+        },
+        //仅保存被选中的具体工作流（叶子）节点的id，以是否具备group属性判断该节点是否为叶子节点
+        selectNode:function (event,node) {
+            console.log(event,node);
+            if(event === 'select'){
+                if(node.group && node.nodes && node.nodes.length > 0){
+                    this.actions.addNodes(node.nodes);
+                }else{
+                    this.selectWorkflow.add(node.id);
+                }
+            }else{
+                if(node.group && node.nodes && node.nodes.length > 0){
+                    this.actions.removeNodes(node.nodes);
+                }else{
+                    this.selectWorkflow.delete(node.id);
+                }
+            }
+        },
+        addNodes:function (nodes) {
+            for(let i=0; i<nodes.length; i++){
+                if(!nodes[i].group){
+                    this.selectWorkflow.add(nodes[i].id);
+                }else{
+                    let children = nodes[i].nodes;
+                    if(children && children.length > 0){
+                        this.actions.addNodes(children);
+                    }
+                }
+            }
+        },
+        removeNodes:function () {
+            for(let i=0; i<nodes.length; i++){
+                if(!nodes[i].group){
+                    this.selectWorkflow.delete(nodes[i].id);
+                }else{
+                    let children = nodes[i].nodes;
+                    if(children && children.length > 0){
+                        this.actions.removeNodes(children);
+                    }
+                }
+            }
+        },
+        setAgentId:function (event) {
+            console.log(this.originData);
+            let user_name = this.el.find("input[name=name_input]").val();
+            for (let agent of this.originData.data.user_list) {
+                if ( user_name === agent.name){
+                    this.selectedAgent = agent.id;
+                }
+            }
+        },
+        setSwitch:function (event) {
+
+        },
+        saveAgent:function (event) {
+            // console.log(this.selectWorkflow);
+            console.log(this.selectedAgent);
+
         }
     },
     afterRender:function () {
         this.actions.initData();
-
+        this.el.on("click","span.save-proxy",(event) => {
+            this.actions.saveAgent(event);
+        }).on("input","input[name=name_input]",(event) => {
+            this.actions.setAgentId(event);
+        }).on("click","input.close-radio",(event) => {
+            this.actions.setSwitch(event);
+        }).on("click","input.open-radio",(event) => {
+            this.actions.setSwitch(event);
+        });
     },
     beforeDestory:function () {
 
