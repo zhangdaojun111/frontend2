5e5dec36ecf89dfa90abc52a99c73c8e72eceacf
qiumaoyun <qiumaoyun@zhongyulian.com>
2017-08-09 14:58:21 +0800

    [bug_fixed]

--- app/entrys/workflow.js
+++ app/entrys/workflow.js
@@ -10,12 +10,10 @@ import WorkFlowCreate from '../components/workflow/workflow-create/workflow-crea
 import WorkflowRecord from '../components/workflow/approval-record/approval-record';
 import WorkflowInitial from '../components/workflow/workflow-initial';
 import WorkFlowForm from '../components/workflow/workflow-form/workflow-form';
+import WorkFlowGrid from '../components/workflow/workflow-grid/workflow-grid';
 import ApprovalHeader from '../components/workflow/approval-header/approval-header';
-
 import ApprovalWorkflow from '../components/workflow/approval-workflow';
-
 import WorkflowAddFollow from '../components/workflow/workflow-addFollow/workflow-addFollow';
-
 import FormEntrys from './form';
 import TreeView from  '../components/util/tree/tree';
 import msgBox from '../lib/msgbox';
@@ -23,7 +21,7 @@ import WorkFlow from '../components/workflow/workflow-drawflow/workflow';
 
 
 WorkFlowForm.showForm();
-
+WorkFlowGrid.showGrid();
 
 let WorkFlowList=workflowService.getWorkfLow({}),
     FavWorkFlowList=workflowService.getWorkfLowFav({});
@@ -36,54 +34,85 @@ Promise.all([WorkFlowList,FavWorkFlowList]).then(res=>{
 HTTP.flush();
 
 //订阅workflow choose事件，获取工作流info并发布getInfo,获取草稿
+let wfObj;
 Mediator.subscribe('workflow:choose', (msg)=> {
+    wfObj=msg;
     (async function () {
         return workflowService.getWorkflowInfo({url: '/get_workflow_info/?seqid=qiumaoyun_1501661055093&record_id=',data:{
             flow_id:msg.id
         }});
-    })().then(res=>{
-            Mediator.publish('workflow:gotWorkflowInfo', res);
-            return workflowService.validateDraftData({form_id:msg.formid});
-        })
-        .then(res=>{
-            if(res.the_last_draft!=''){
-                $( "#dialog-confirm" ).dialog({
-                    title:'提示',
-                    resizable: false,
-                    height: "auto",
-                    width: 400,
-                    modal: true,
-                    buttons: {
-                        "确认": function() {
-                            $( this ).dialog( "close" );
-                            //todo get draft info
-                            $("#dialog-confirm").html('');
-                        },
-                        "取消": function() {
-                            $( this ).dialog( "close" );
-                            $("#dialog-confirm").html('');
-                        }
-                    }
-                });
-                $("#dialog-confirm").append(`<p><span class="ui-icon ui-icon-alert"></span>
-                    您于${res.the_last_draft}时填写该工作表单尚未保存，是否继续编辑？
-                </p>`);
-
-            }else{
-                alert('there is no draft');
-            }
-            $("#workflow-create").append(`<button id="submit" class="ui-button ui-widget ui-corner-all">提交</button>`);
-        }).then(()=>{
-        console.log(`tableid:${msg.tableid}`);
-        // FormEntrys.createForm({
-        //     table_id:msg.tableid,
-        //     el:'#place-form',
-        //     is_view:1,
-        //     real_id:''
-        // });
+    })()
+    .then(res=>{
+        Mediator.publish('workflow:gotWorkflowInfo', res);
+        return workflowService.validateDraftData({form_id:msg.formid});
     })
+    .then(res=>{
+        if(res.the_last_draft!=''){
+            return msgBox.confirm(`您于${res.the_last_draft}时填写该工作表单尚未保存，是否继续编辑？`)
+        }else{
+            return 0;
+        }
+    })
+    .then((is_draft)=>{
+        //auto saving draft  草稿自动保存
+        is_draft=is_draft==true?1:0;
+        $('#place-form').html('');
+        FormEntrys.createForm({
+            reload_draft_data:is_draft,
+            table_id:msg.tableid,
+            el:'#place-form',
+            real_id:'',
+            from_workflow:1,
+            form_id:msg.formid
+        });
 
+        const intervalSave= async function (data) {
+            let postData={
+                flow_id:msg.id,
+                is_draft:1,
+                data:{}
+            };
+            postData.data=JSON.stringify(data);
+            let res = await workflowService.createWorkflowRecord(postData);
+            console.log(res);
+            if(res.success===1){
+                msgBox.alert('自动保存成功！');
+            }
+        };
+        var timer;
+        const autoSaving=function(){
+            timer=setInterval(()=>{
+                intervalSave(FormEntrys.getFormValue());
+            },2*60*1000);
+        };
+        // autoSaving();
+        Mediator.subscribe('workflow:autoSaveOpen', (msg)=> {
+            clearInterval(timer);
+            if(msg===1){
+                autoSaving();
+            }else{
+                clearInterval(timer);
+            }
+        })
+    })
+    
 });
+
+Mediator.subscribe('workflow:submit', (res)=> {
+    let formData=FormEntrys.getFormValue(),
+        postData={
+        flow_id:wfObj.id,
+        is_draft:1,
+        focus_users:JSON.stringify(res)||[],
+        data:JSON.stringify(formData)
+    };
+    (async function () {
+        let data = await workflowService.createWorkflowRecord(postData);
+        alert(`error:${data.error}`);
+    })();
+});
+
+
 
 //订阅收藏常用workflow
 Mediator.subscribe('workflow:addFav', (msg)=> {
@@ -103,29 +132,25 @@ Mediator.subscribe('workflow:delFav', (msg)=> {
 //审批工作流
 
 var mockFlowData;
-ApprovalHeader.showheader();
 
 (async function () {
     return workflowService.getWorkflowInfo({url: '/get_workflow_info/?seqid=qiumaoyun_1501661055093&record_id=',data:{
         flow_id:30
     }});
 })().then(res=>{
-    console.log(res);
-    Mediator.publish('workflow:gotImgInfo', res);
     Mediator.publish('workflow:gotWorkflowInfo', res);
     mockFlowData=res;
 });
 
 let tree=[];
+let staff=[];
 (async function () {
     return workflowService.getStuffInfo({url: '/save_perm/?perm_id=0'});
 })().then(res=>{
     tree=res.data.department_tree;
-
+    staff=res.data.department2user;
     function recur(data) {
-        console.log(data);
         for (let item of data){
-            console.log(item);
             item.nodes=item.children;
             if(item.children.length!==0){
                 recur(item.children);
@@ -133,6 +158,8 @@ let tree=[];
         }
     }
     recur(tree);
+    console.log(staff);
+    
 
     var treeComp2 = new TreeView(tree,{
         callback: function (event,selectedNode) {
@@ -183,7 +210,7 @@ let tree=[];
 });
 
 
-    
+
 FormEntrys.createForm({
     el:"#place-form",
     form_id:181,
@@ -193,8 +220,8 @@ FormEntrys.createForm({
     from_focus:0,
     table_id:'5318_EHFuJD7Ae76c6GMPtzdiWH'
 }).then((res)=>{
-    ApprovalHeader.showheader(res);
-    WorkflowRecord.showRecord(res);
+        ApprovalHeader.showheader(res);
+        WorkflowRecord.showRecord(res);
 });
 // function getRecordObj () {
 //     return new Promise(function (resolve, reject) {
@@ -219,11 +246,13 @@ FormEntrys.createForm({
 //        console.log(res)
 // })
 
+
+
+
 //获取盖章图片
 Mediator.subscribe("workflow:getStampImg",(msg)=>{
     (async function () {
         let data = await workflowService.getStmpImg(msg);
-        console.log(msg)
     })();
 });
 
