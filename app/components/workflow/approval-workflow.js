58e64d9f07a9f76ad6da4ea0c6936817c10ab243
hufei <591541804@qq.com>
2017-08-09 18:45:30 +0800

    拖拽

--- app/components/workflow/approval-workflow.js
+++ app/components/workflow/approval-workflow.js
@@ -16,23 +16,23 @@ let config={
         node_id:null
     },
     actions:{
-        // approveWorkflow (__this){
-        //     (async function () {
-        //         return workflowService.approveWorkflowRecord({
-        //             url: '/approve_workflow_record/?seqid=qiumaoyun_1502093694205&record_id=598819d246e8e4283ced51bd',
-        //             data:__this.data
-        //         });
-        //     })().then(res=>{
-        //         console.log(res);
-        //     })
-        // },
+        approveWorkflow (__this){
+            (async function () {
+                return workflowService.approveWorkflowRecord({
+                    url: '/approve_workflow_record/?seqid=qiumaoyun_1502093694205&record_id=598819d246e8e4283ced51bd',
+                    data:__this.data
+                });
+            })().then(res=>{
+                console.log(res);
+            })
+        },
         previewView:function (el,appendDiv,addFollow) {
             let type=$(el).data("preview");
 
 
             switch (type){
                 case 'follow-view' :
-                    let addFollow=this.el.find(".workflow-foot #add-follow").clone(true);
+                    let addFollow=this.el.find(".workflow-foot #add-follow").clone();
                     appendDiv.find(".preview-node1").html(addFollow);
                     appendDiv.find(".preview-node1").toggle().siblings().hide();
                     break;
@@ -42,34 +42,10 @@ let config={
                     appendDiv.find(".preview-node2").toggle().siblings().hide();
                     break;
                 case 'record-view' :
-                    let workflowRecord=this.el.find(".workflow-record #workflow-record").clone();
+                    let workflowRecord=this.el.find(".workflow-foot #workflow-record").clone();
                     appendDiv.find(".preview-node3").html(workflowRecord);
                     appendDiv.find(".preview-node3").toggle().siblings().hide();
                     break;
-            }
-        },
-        recordFn:function () {
-            let kind={
-                pass:function () {
-                    msgBox.confirm("你确定审核通过吗").then((res)=>{
-                         if(res){
-                             Mediator.publish('approval:recordPass',res);
-                             // console.log('提交成功')
-                         }else {
-                             // console.log('未提交')
-                             return;
-                        }
-                    })
-                }
-            };
-            return kind;
-        },
-        toogz(e){
-            let ev = this.el.find(".signature");
-            if(ev.css("display")=="none"){
-                ev.css("display","block");
-            }else{
-                ev.css("display","none");
             }
         }
 
@@ -79,6 +55,7 @@ let config={
         Mediator.subscribe('workflow:gotWorkflowInfo', (msg)=> {
             WorkFlow.show(msg.data[0],'#drawflow');
         });
+
         // this.el.on('click','#app-pass',()=>{
         //     this.actions.approveWorkflow(__this);
         // });
@@ -86,12 +63,15 @@ let config={
         this.el.on('click','.gz',(e)=>{
             this.actions.toogz(e);
         })
+
+        this.el.on('click','#app-pass',()=>{
+            this.actions.approveWorkflow(__this);
+        });
+
+
         this.el.on('click',".preview-btn",function () {
             let appendDiv=__this.el.find("#preview-node");
             __this.actions.previewView($(this),appendDiv);
-        })
-        this.el.on('click','#app-pass',function () {
-            __this.actions.recordFn().pass()
         })
 
 
@@ -106,7 +86,6 @@ class ApprovalWorkflow extends Component{
 let component = new ApprovalWorkflow();
 let el = $('#approval-workflow');
 component.render(el);
-Mediator.subscribe("workflow:getStampImg",(msg)=>{
-    console.log(msg);
-    WorkflowSeal.showheader(msg);
-})
+
+let data = {"file_ids": ["5987de19c3ec2134050ee679", "5987de3244543b4d1226c977", "5987fe3e8e368f5747b1722c"]}
+WorkflowSeal.showheader(data);
