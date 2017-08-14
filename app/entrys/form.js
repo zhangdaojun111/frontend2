21b52e839dfd69ab8b772d6bd2120d37ab5ab8d4
yudeping <yudeping@wanwujieshu.net>
2017-08-14 17:26:38 +0800

    []

--- app/entrys/form.js
+++ app/entrys/form.js
@@ -1,5 +1,4 @@
 import FormBase from '../components/form/base-form/base-form'
-import {HTTP} from '../lib/http';
 import Mediator from '../lib/mediator';
 import {FormService} from "../services/formService/formService";
 import '../components/form/base-form/base-form.scss'
@@ -11,7 +10,6 @@ let FormEntrys = {
         this.tableId='';
         this.parentRealId='';
         this.parentTempId='';
-        this.seqId='';
         this.realId='';
         this.parentTableId='';
         this.parentRecordId='';
@@ -35,7 +33,6 @@ let FormEntrys = {
         this.tableId=config.table_id||'';
         this.parentRealId=config.parent_real_id||'';
         this.parentTempId=config.parent_temp_id||'';
-        this.seqId=config.seqId||'';
         this.realId=config.real_id||'';
         this.parentTableId=config.parent_table_id||'';
         this.parentRecordId=config.parent_record_id||'';
@@ -56,7 +53,8 @@ let FormEntrys = {
         this.buildId=config.buildId || '';
         this.btnType=config.btnType||'new';
     },
-    hasKeyInFormDataStatic:function (key,staticData){
+    //静态数据里是否有这个key
+    hasKeyInFormDataStatic(key,staticData){
     let isExist = false;
     for(let dict of staticData["data"]){
         if(dict["dfield"] == key){
@@ -68,8 +66,6 @@ let FormEntrys = {
     //找到加载表单数据的formId和加载节点的flowId
     findFormIdAndFlowId(res) {
         if(res["data"] && res["data"]["flow_data"].length != 0) {
-            //给选择节点视图的下拉框赋值
-            this.selectItems = res["data"]["flow_data"];
             //默认的form_id和flow_id取第一个select
             this.formId = res["data"]["flow_data"][0]["form_id"];
             this.flowId = res["data"]["flow_data"][0]["flow_id"];
@@ -83,9 +79,6 @@ let FormEntrys = {
         }
         if(res["data"] && res["data"]["form_id"] != 0){
             this.formId = res["data"]["form_id"];
-            this.isloadCustomTableForm = true;
-        }else {
-            this.isloadWorkflow = true;
         }
     },
     //拼装发送json
@@ -114,6 +107,7 @@ let FormEntrys = {
         }
         return json;
     },
+    //非工作流请求json
     pickJson() {
         let json = {};
         if(this.fieldId !== ""){
@@ -155,50 +149,49 @@ let FormEntrys = {
         }
         return json;
     },
-    //merge数据
-    mergeFormData:function (staticData,dynamicData){
-    for(let dfield in dynamicData["data"]){
-        if(this.hasKeyInFormDataStatic(dfield,staticData)){
-            for(let dict of staticData["data"]){
-                if(dict["dfield"] == dfield){
-                    for(let k in dynamicData["data"][dfield]){
-                        dict[k] = dynamicData["data"][dfield][k];
+    //merge static和dynamic数据
+    mergeFormData(staticData,dynamicData){
+        for(let dfield in dynamicData["data"]){
+            if(this.hasKeyInFormDataStatic(dfield,staticData)){
+                for(let dict of staticData["data"]){
+                    if(dict["dfield"] == dfield){
+                        for(let k in dynamicData["data"][dfield]){
+                            dict[k] = dynamicData["data"][dfield][k];
+                        }
                     }
                 }
+            }else{
+                staticData["data"].push(dynamicData["data"][dfield]);
             }
-        }else{
-            staticData["data"].push(dynamicData["data"][dfield]);
         }
-    }
-    staticData["record_info"] = dynamicData["record_info"];
-    staticData["parent_table_id"] = dynamicData["parent_table_id"];
-    staticData["frontend_cal_parent_2_child"] = dynamicData["frontend_cal_parent_2_child"];
-    staticData["error"] = dynamicData["error"];
-    let data={};
-    if(!this.formId || staticData['form_id'] == this.formId){
-        this.parseRes(staticData);
-    }
-    staticData.formData=staticData.data;
-    for(let obj of staticData.data){
-        data[obj.dfield]=obj;
-    }
-    staticData.data=data;
-
-    staticData['temp_id']=staticData.data['temp_id']||'';
-    staticData['real_id']=staticData.data['real_id']||'';
-    staticData['table_id']=staticData.data['table_id']||'';
-    staticData['parentRealId']=staticData["real_id"]["value"]||'';
-    staticData['parentTableId']=staticData["table_id"]["value"]||'';
-    staticData['parentTempId']=staticData["temp_id"]["value"]||'';
-    staticData.tableId=staticData['table_id']["value"];
-    staticData.formId=this.formId;
-    staticData.realId=this.realId;
-    staticData.flowId=this.flowId;
-    staticData.isBatch=this.isBatch;
-    staticData.key=this.key;
-    staticData.btnType=this.btnType;
-    return staticData;
-},
+        staticData["record_info"] = dynamicData["record_info"];
+        staticData["parent_table_id"] = dynamicData["parent_table_id"];
+        staticData["frontend_cal_parent_2_child"] = dynamicData["frontend_cal_parent_2_child"];
+        staticData["error"] = dynamicData["error"];
+        let data={};
+        if(!this.formId || staticData['form_id'] == this.formId){
+            this.parseRes(staticData);
+        }
+        staticData.formData=staticData.data;
+        for(let obj of staticData.data){
+            data[obj.dfield]=obj;
+        }
+        staticData.data=data;
+        staticData['temp_id']=staticData.data['temp_id']||'';
+        staticData['real_id']=staticData.data['real_id']||'';
+        staticData['table_id']=staticData.data['table_id']||'';
+        staticData['parentRealId']=staticData["real_id"]["value"]||'';
+        staticData['parentTableId']=staticData["table_id"]["value"]||'';
+        staticData['parentTempId']=staticData["temp_id"]["value"]||'';
+        staticData.tableId=staticData['table_id']["value"];
+        staticData.formId=this.formId;
+        staticData.realId=this.realId;
+        staticData.flowId=this.flowId;
+        staticData.isBatch=this.isBatch;
+        staticData.key=this.key;
+        staticData.btnType=this.btnType;
+        return staticData;
+    },
     //处理字段数据
     parseRes (res){
     if(res !== null){
@@ -247,31 +240,33 @@ let FormEntrys = {
         }
     }
 },
-    //默认表单
+    //创建默认表单
     formDefaultVersion : function (data){
-    let html=`<table class="form table table-striped table-bordered table-hover ">
-            <tbody>
-                `;
-    for(let obj of data){
-        if(data.type==='hidden'){
-            html+=`<div data-dfield="${obj.dfield}" data-type="${obj.type}"></div>`;
-        }else{
-            html+=`<tr>
-                        <td style="width: 150px;white-space: nowrap;">${ obj.label }</td>
-                        <td><div data-dfield="${obj.dfield}" data-type="${obj.type}"></div></td>
-                </tr>`;
+        let html=`<table class="form table table-striped table-bordered table-hover ">
+                <tbody>
+                    `;
+        for(let obj of data){
+            if(data.type==='hidden'){
+                html+=`<div data-dfield="${obj.dfield}" data-type="${obj.type}"></div>`;
+            }else{
+                html+=`<tr>
+                            <td style="width: 150px;white-space: nowrap;">${ obj.label }</td>
+                            <td><div data-dfield="${obj.dfield}" data-type="${obj.type}"></div></td>
+                    </tr>`;
+            }
         }
-    }
-    html+=`</tbody>
-        </table>`
+        html+=`</tbody>
+            </table>`
     return html;
-},
+    },
+    //清除所有已建form
     destoryAll(){
         for(let key in this.childForm){
             this.childForm[key].destroySelf();
             delete this.childForm[key];
         }
     },
+    //销毁单个form实例
     destoryForm(tableID){
         if(this.childForm[tableID]){
             this.childForm[tableID].destroySelf();
@@ -279,38 +274,37 @@ let FormEntrys = {
         }
     },
     //创建表单入口
-    createForm(config={}){
+    async createForm(config={}){
         let _this=this;
         this.init(config);
-        let tableID=this.tableId;
         let html=$(`<div id="detail-form" style="" class="table-wrap wrap">`).prependTo(this.el);
-        let template='<table><tbody><tr class="firstRow"><td width="244" valign="top"><span data-id="2562_nLNdMCPYogJJ4py4AHqDum" style="border:2px">名称</span></td><td width="244" valign="top"><label id="2562_nLNdMCPYogJJ4py4AHqDum" style="border:2px"><input type="text" data-fill-in="0" style="box-sizing:border-box;width:240px;height:34px;line-height:34px;border-radius:5px;padding:6px 12px;border:1px solid #ccc;" name="2562_nLNdMCPYogJJ4py4AHqDum" data-required="0"/></label></td><td width="244" valign="top" style="word-break: break-all;"><br/></td><td width="244" valign="top" style="word-break: break-all;"><br/></td></tr><tr><td width="244" valign="top" style="word-break: break-all;"><span data-id="7949_yaq4qmVjgatey4xAi2UCT9" style="border:2px">年份</span></td><td width="244" valign="top" style="word-break: break-all;"><label id="7949_yaq4qmVjgatey4xAi2UCT9" style="border:2px"><select data-fill-in="1" style="box-sizing:border-box;width:240px;height:34px;line-height:34px;border-radius:5px;border:1px solid #ccc;" name="7949_yaq4qmVjgatey4xAi2UCT9" data-required="0" data-year="1" class="normalSelect"></select></label></td><td width="244" valign="top"><span data-id="4207_jUwup8ziqYyTyeMivJJ2JL" style="border:2px">所在地</span></td><td width="244" valign="top"><label id="4207_jUwup8ziqYyTyeMivJJ2JL" style="border:2px"><input type="radio" data-required="0" data-fill-in="2" name="4207_jUwup8ziqYyTyeMivJJ2JL" value="6971_oargmg9mnTxZTU2Qqo6uge"/>北京<input type="radio" data-required="0" data-fill-in="2" name="4207_jUwup8ziqYyTyeMivJJ2JL" value="9398_ysjjqkNsbkf8A6yRar8Fsg"/>深圳<input type="radio" data-required="0" data-fill-in="2" name="4207_jUwup8ziqYyTyeMivJJ2JL" value="4253_5eN7tuKuBL2tLgiVPMhxAj"/>上海<input type="radio" data-required="0" data-fill-in="2" name="4207_jUwup8ziqYyTyeMivJJ2JL" value="1197_gP79KY5yjLLXFGvWF4JkBB"/>成都</label></td></tr></tbody></table><p><br/></p>';
-        FormService.getPrepareParmas({table_id:this.tableId}).then(res=>{
-            _this.findFormIdAndFlowId(res);
-            let json=_this.createPostJson();
-            FormService.getFormData(json).then(res=>{
-                console.time('form创建时间');
-                if(_this.fromApprove){
-                    if(res[1]['record_info']){
-                        Mediator.publish('workFlow:record_info',res[1]['record_info']);
-                    }
-                }
-                if(_this.formId){
-                    template=res[2]['data']['content'];
-                }else{
-                    template=_this.formDefaultVersion(res[0].data);
-                }
-                let data=_this.mergeFormData(res[0],res[1]);
-                let formData={
-                    template:template,
-                    data:data,
-                }
-                let formBase=new FormBase(formData);
-                _this.childForm[_this.tableId]=formBase;
-                formBase.render(html);
-                console.timeEnd('form创建时间');
-            });
-        })
+        let res=await  FormService.getPrepareParmas({table_id:this.tableId});
+        _this.findFormIdAndFlowId(res);
+        let json=_this.createPostJson();
+        res =await FormService.getFormData(json);
+        console.time('form创建时间');
+        //发送审批记录
+        if(_this.fromApprove){
+            if(res[1]['record_info']){
+                Mediator.publish('workFlow:record_info',res[1]['record_info']);
+            }
+        }
+        let template;
+        if(_this.formId){
+            //手绘表单
+            template=res[2]['data']['content'];
+        }else{
+            template=_this.formDefaultVersion(res[0].data);
+        }
+        let data=_this.mergeFormData(res[0],res[1]);
+        let formData={
+            template:template,
+            data:data,
+        }
+        let formBase=new FormBase(formData);
+        _this.childForm[_this.tableId]=formBase;
+        formBase.render(html);
+        console.timeEnd('form创建时间');
     },
 
     //审批删除时重置表单可编辑性
@@ -326,6 +320,7 @@ let FormEntrys = {
         this.childForm[tableId].data.focus_users=data;
     },
 
+    //获取表单数据
     getFormValue(tableId){
         if(!this.childForm[tableId]){
             return;
@@ -333,126 +328,4 @@ let FormEntrys = {
         return this.childForm[tableId].actions.getFormValue();
     }
 }
-
-$('#toEdit').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        table_id:'8696_yz7BRBJPyWnbud4s6ckU7e',
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId
-    });
-});
-$('#text').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        table_id:'1285_pkz2teyhHCztFrYhoc6F54',
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId
-    });
-});
-$('#count').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        table_id:'8390_35R9y7J5uVULgczYyZvqvB',
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId
-    });
-});
-$('#editRequired').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        table_id:'3461_P28RYPGTGGE7DVXH8LBMHe',
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId
-    });
-});
-$('#defaultValue').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        table_id:'1160_ex7EbDsyoexufF2UbXBmSJ',
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId
-    });
-});
-$('#valid').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        table_id:'2638_urGGDDp75VvymeqWj3eo6F',
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId
-    });
-});
-$('#exp').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId,
-        table_id:'7336_HkkDT7bQQfqBag4kTiFWoa'
-    });
-
-});
-$('#date').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId,
-        table_id:'9890_AbtdVuR9umBCSwtjaRTu6D'
-    });
-
-});
-$('#workflow').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        table_id:'449_6k2VdLn4ArCfgFPuAjFrNQ',
-        seqId:'yudeping',
-        el:$('body'),
-        is_view:isView,
-        real_id:realId
-    });
-})
-$('#lalala').on('click',function(){
-    let realId=$('#real_id').val()||'';
-    let isView=$('#is_view').val()||0;
-    let tableId=$('#tableId').val()||0;
-    FormEntrys.destoryAll();
-    FormEntrys.createForm({
-        table_id:tableId,
-        seqId:'zengjing',
-        el:$('body'),
-    });
-})
 export default FormEntrys
