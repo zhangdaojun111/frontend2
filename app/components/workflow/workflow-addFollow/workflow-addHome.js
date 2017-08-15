e3bce3948619a400acf9b9b3ca35047f530adeb9
yincaihua <249410592@qq.com>
2017-08-15 13:38:08 +0800

    更新bi_test

---
+++ app/components/workflow/workflow-addFollow/workflow-addHome.js
@@ -0,0 +1,58 @@
+import Component from '../../../lib/component';
+import './workflow-addFollow.scss';
+import Mediator from '../../../lib/mediator';
+import {PMAPI,PMENUM} from '../../../lib/postmsg';
+
+let config={
+    template: `<div class="flex flex-start">
+    <div class="workflow-box-l">
+        <div class="tit">
+            <span>关注人</span>
+            <i></i>
+        </div>
+    </div>
+    <div class="workflow-box-r">
+        <div class="content">
+            <div class="add-follow" id="addFollower">添加关注人</div>
+            <div class="follow-name-list" id="addFollowerList">
+                
+            </div>
+        </div>
+    </div>
+   
+</div>`,
+    data:{},
+    action:{
+
+    },
+    afterRender(){
+        this.el.on('click','#addFollower',()=>{
+            PMAPI.openDialogByIframe(`/iframe/addFocus/`,{
+                width:1000,
+                height:800,
+                title:`添加关注人`,
+                modal:true
+            }).then(res=>{
+                if(!res.onlyclose){
+                    let nameArr=[],idArr=[];
+                    for(var k in res){
+                        nameArr.push(res[k]);
+                        idArr.push(k);
+                    }
+                    this.el.find('#addFollowerList').text(nameArr);
+                    Mediator.publish('workflow:focus-users',idArr);
+                }
+            })
+        });
+   
+    }
+};
+class WorkflowAddFollow extends Component{
+    constructor (data){
+        super(config,data);
+    }
+}
+
+let component = new WorkflowAddFollow();
+let el = $('#add-home');
+component.render(el);
