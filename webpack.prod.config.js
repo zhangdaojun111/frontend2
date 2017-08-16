441e15aa37f385279bf98009559fdc4cec83c127
XiongXiaotao <xxtao110@163.com>
2017-08-16 21:04:37 +0800

    bug

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -45,7 +45,6 @@ module.exports = {
             'jquery-ui/themes/base/theme.css',
             'mediator-js',
             'handlebars',
-            'moment',
             'lodash',
             'babel-polyfill'
         ]
