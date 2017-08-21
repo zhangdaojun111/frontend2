1635db606013cb521ce26be69ef36c292421a7f3
zhaohaoran <zhaohaoran44@126.com>
2017-08-21 17:02:34 +0800

    []

---
+++ app/components/dataGrid/data-table-toolbar/history-approve-data/history-approve-HisTable/history-approve-HisItem/history-approve-HisItem.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zhaohaoran
+ */
+import Component from "../../../../../../lib/component";
+import template from './history-approve-HisItem.html'
+
+
+let config = {
+    template: template,
+    data: {
+    },
+    actions: {
+
+    },
+    afterRender: function() {
+    }
+}
+class historyTable extends Component {
+    constructor(data) {
+        config.data = data
+        super(config)
+    }
+}
+export default historyTable
