fe28ecd2e065d2b2ce26343e33bca6c75f782cc7
XiongXiaotao <xxtao110@163.com>
2017-08-23 10:48:19 +0800

    bugfix

--- app/lib/postmsg.js
+++ app/lib/postmsg.js
@@ -36,9 +36,10 @@ export const PMENUM = {
     iframe_silent: '5',
     table_invalid: '6',              // 表格数据失效
     on_the_way_invalid: '7',         // 在途数据失效
+    data_invalid: '11',
     open_iframe_params: '8',
     get_param_from_root: '9',        // 来自子框架的消息，需要获取iframe的参数
-    send_param_to_iframe: '10'       // 来组主框架的消息，向iframe发送参数
+    send_param_to_iframe: '10',       // 来组主框架的消息，向iframe发送参数
 }
 
 /**
