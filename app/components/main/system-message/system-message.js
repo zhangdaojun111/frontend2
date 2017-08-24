d5567207bb104c70160d489316c3013a3b88f9f2
XiongXiaotao <xxtao110@163.com>
2017-08-24 15:18:46 +0800

    代码优化

--- app/components/main/system-message/system-message.js
+++ app/components/main/system-message/system-message.js
@@ -23,12 +23,13 @@ let config = {
                 first: (this.pagination.data.currentPage - 1) * this.pagination.data.rows,
                 currentPage: this.pagination.data.currentPage
             });
-            console.log(this.pagination.data.currentPage);
+            this.showLoading();
             systemMessageService.getMyMsg(param).then((data) => {
                 this.agGrid.actions.setGridData({
                     rowData: data.rows
                 });
                 this.pagination.actions.setPagination(data.total, param.currentPage);
+                this.hideLoading();
             });
         },
         markRead: function () {
