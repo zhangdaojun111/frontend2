e37db22444aa9a15d30eface6ae4defebe26f619
ZY <510980866@qq.com>
2017-08-29 10:59:05 +0800

    global search UI

--- app/components/main/search-display/file-display/file-display.js
+++ app/components/main/search-display/file-display/file-display.js
@@ -17,8 +17,8 @@ let config = {
     },
     actions:{
         initInfo:function () {
-            this.el.find('div.table-name').html(this.data.searchData.label);
-            this.el.find('p.attachment-name').html(this.data.searchData.file_name + " 附件");
+            // this.el.find('div.table-name').html(this.data.searchData.label);
+            this.el.find('div.attachment-content').html(this.data.searchData.file_name + " 附件");
             this.actions.setHref();
         },
         setHref:function () {
