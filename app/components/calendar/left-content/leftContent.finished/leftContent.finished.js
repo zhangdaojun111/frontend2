7322ff43acd77950df6cfda6681aba21f7a4b935
lipengfei <love024>
2017-08-29 16:07:16 +0800

    Merge branch 'feature_dev_1.0_calendar' of https://gitlab.erdstest.com/erds/frontend2 into calendar-lpf
    # Conflicts:
    #	app/components/calendar/left-content/left-content.js

--- app/components/calendar/left-content/leftContent.finished/leftContent.finished.js
+++ app/components/calendar/left-content/leftContent.finished/leftContent.finished.js
@@ -4,7 +4,8 @@
  */
 import Component from "../../../../lib/component";
 import template from './leftContent.finished.html';
-import './leftContent.finished.scss'
+import './leftContent.finished.scss';
+import {PMAPI} from '../../../../lib/postmsg';
 import {CalendarService} from '../../../../services/calendar/calendar.service';
 import Mediator from '../../../../lib/mediator';
 let config = {
@@ -26,6 +27,16 @@ let config = {
     ],
     afterRender: function() {
         console.log(this.data);
+        this.el.on('click', '.finished-content', () => {
+            PMAPI.openDialogByIframe(
+                `/wf/approval/?record_id=${this.data['id']}&form_id=${this.data['form_id']}&table_id=${this.data['table_id']}&flow_id=${this.data['flow_id']}`,
+                {
+                    width: "100%",
+                    height: '900',
+                    modal: true,
+                    customSize: true,
+                })
+        })
     },
 }
 class leftContentFinished extends Component {
