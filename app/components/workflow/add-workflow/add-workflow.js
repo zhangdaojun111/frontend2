5092341b17283ab76c3918cb9b77d2bce66df52a
qiumaoyun <qiumaoyun@zhongyulian.com>
2017-08-30 17:46:09 +0800

    [bugfix]

--- app/components/workflow/add-workflow/add-workflow.js
+++ app/components/workflow/add-workflow/add-workflow.js
@@ -53,13 +53,17 @@ let config={
                 htmlStr+=`<option data-default=${res[i].selected} data-flow_id=${res[i].flow_id} data-form_id=${res[i].form_id}>${res[i].flow_name}</option>`;
             }
             this.el.find('#wf-select').html(htmlStr);
+            let o={};
+            o.flow_id=_this.el.find('#wf-select option[data-default="1"]').data('flow_id');
+            o.form_id=_this.el.find('#wf-select option[data-default="1"]').data('form_id');
+            this.el.find('#wf-select option[data-default="1"]').attr("selected",true);
+            Mediator.publish('workflow:getflows', o);
             this.el.find('#wf-select').on('change',()=>{
                 let o={};
-                o.flow_id=_this.el.find('#wf-select option[data-default="1"]').data('flow_id');
-                o.form_id=_this.el.find('#wf-select option[data-default="1"]').data('form_id');
-                _this.el.find('#wf-select option[data-default="1"]').attr("selected",true);
+                o.flow_id=this.el.find('#wf-select option:selected').data('flow_id');
+                o.form_id=this.el.find('#wf-select option:selected').data('form_id');
                 Mediator.publish('workflow:getflows', o);
-            }).trigger('change');
+            });
         });
         this.el.find('#subAddworkflow').on('click',()=>{
             Mediator.publish('workflow:submit', 1);
