30a63fcb91e0f8ee55c1e0cb98d6e73c3da964ba
yudeping <yudeping@wanwujieshu.net>
2017-08-30 17:00:24 +0800

    []

--- app/entrys/popup/choose.js
+++ app/entrys/popup/choose.js
@@ -3,17 +3,8 @@
  *选择器入口
  */
 import BuildChoose from '../../components/popup/buildChoose/buildChoose';
-// function GetQueryString(name)
-// {
-//     let reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-//     let r = window.location.search.substr(1).match(reg);
-//     if(r!=null)return  unescape(r[2]); return null;
-// }
 $(document).ready(function(){
-    let fieldId=GetQueryString('fieldId');
-    let key=GetQueryString('key');
     let el=$('<div></div>').appendTo($('body'));
-    // let buildChoose=new BuildChoose({fieldId:fieldId,key:key});
     let buildChoose=new BuildChoose(window.config);
     buildChoose.render(el);
 });
