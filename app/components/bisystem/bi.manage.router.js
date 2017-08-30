a50ab30c43ecadebfc052a8ddd11eb9d1c81221a
yincaihua <249410592@qq.com>
2017-08-30 10:05:59 +0800

    修改表单配置

--- app/components/bisystem/bi.manage.router.js
+++ app/components/bisystem/bi.manage.router.js
@@ -9,7 +9,7 @@ import {FormEntryComponent} from './forms/entry/entry';
 import {componentsJson} from './forms/entry/loadFormChart.json';
 import Mediator from '../../lib/mediator';
 
-import {LineBarEditor} from './test/editors/linebar/linebar';
+// import {LineBarEditor} from './test/editors/linebar/linebar';
 let component;
 let viewComponent;
 let formComponent = {};
@@ -61,12 +61,12 @@ const BiAppRouter = Backbone.Router.extend({
             formComponent[type].reset(comType);
             formComponent[type].reload();
         } else {
-            // let component = new componentsJson[type]['component'](comType);
-            // component.render($('#route-outlet'));
-            // formComponent[type] = component;
-            component = new LineBarEditor();
+            let component = new componentsJson[type]['component'](comType);
             component.render($('#route-outlet'));
-            component.drawForm();
+            formComponent[type] = component;
+            // component = new LineBarEditor();
+            // component.render($('#route-outlet'));
+            // component.drawForm();
         }
 
     }
