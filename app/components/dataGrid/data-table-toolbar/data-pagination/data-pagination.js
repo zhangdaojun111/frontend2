38cc44c481f801bed4fdf74e9cb8870d4bd81542
yangxiaochuan <yangxiaochuan@wanwujieshu.net>
2017-09-07 14:27:19 +0800

    [bugfix]分页

--- app/components/dataGrid/data-table-toolbar/data-pagination/data-pagination.js
+++ app/components/dataGrid/data-table-toolbar/data-pagination/data-pagination.js
@@ -175,6 +175,8 @@ let config = {
                 this.actions.onPaginationChanged();
             },1000 ) ).on( 'click','.ui-icon-refresh',()=>{
                 this.actions.onPaginationChanged();
+            } ).on( 'click','.data-invalid',()=>{
+                this.el.find( '.data-invalid' ).eq(0).html( '' );
             } )
         },
         onPaginationChanged: function (invalid) {
