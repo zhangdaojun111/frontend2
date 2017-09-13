10f7976cffa1ec356f7305f94e94edf64521b500
qiumaoyun <qiumaoyun@zhongyulian.com>
2017-09-13 16:42:26 +0800

    []

--- app/entrys/workflowForGrid.js
+++ app/entrys/workflowForGrid.js
@@ -16,8 +16,6 @@ import WorkFlowForm from '../components/workflow/workflow-form/workflow-form';
 import WorkFlowGrid from '../components/workflow/workflow-grid/workflow-grid';
 import ApprovalHeader from '../components/workflow/approval-header/approval-header';
 import ApprovalWorkflow from '../components/workflow/approval-workflow/approval-workflow';
-import WorkflowAddFollow from '../components/workflow/workflow-addFollow/workflow-addFollow/workflow-addFollow';
-import WorkflowAddSigner from '../components/workflow/workflow-addFollow/workflow-addSigner/workflow-addSigner';
 import FormEntrys from './form';
 import TreeView from '../components/util/tree/tree';
 import msgBox from '../lib/msgbox';
@@ -41,7 +39,7 @@ const workflowForGrid={
 
     },
     create(){
-        WorkflowAddFollow.showAdd();
+        $('#addFollower').hide();
         WorkFlowForm.showForm();
         
         let nameArr=[],focus=[],is_view,tree=[],staff=[];
@@ -75,107 +73,6 @@ const workflowForGrid={
                         focus.push(a[i][j]);
                     }
                 }
-                if(focus.length>0){
-                    let dept=[];
-                    (async function () {
-                        return workflowService.getWorkflowInfo({url: '/get_all_users/'});
-                    })().then(users => {
-                        let idArr=[];
-                        for(var i in focus){
-                            idArr.push(users.rows[focus[i]].id);
-                            dept.push(users.rows[focus[i]].department);
-                        }
-                        Mediator.publish('workflow:idArr', idArr);
-                        dept=_.uniq(dept);
-                    }).then(()=>{
-                        (async function () {
-                            return workflowService.getStuffInfo({url: '/get_department_tree/'});
-                        })().then(res=>{
-                            tree=res.data.department_tree;
-                            staff=res.data.department2user;
-                            function recur(data) {
-                                for (let item of data){
-                                    item.nodes=item.children;
-                                    for(let i in dept){
-                                        if(item.text.indexOf(dept[i])!==-1){
-                                            item.state={};
-                                            item.state.checked=true;
-                                            item.state.selected=true;
-                                            for(var k in staff){
-                                                if(k==item.id){
-                                                    Mediator.publish('workflow:checkDeptAlready', staff[k]);
-                                                }
-                                            }
-                                        }
-                                    }
-                                    if(item.children.length!==0){
-                                        recur(item.children);
-                                    }
-                                }
-                            }
-                            recur(tree);
-                            var treeComp2 = new TreeView(tree,{
-                                callback: function (event,selectedNode) {
-                                    if(event==='select'){
-                                        for(var k in staff){
-                                            if(k==selectedNode.id){
-                                                Mediator.publish('workflow:checkDept', staff[k]);
-                                            }
-                                        }
-                                    }else{
-                                        for(var k in staff){
-                                            if(k==selectedNode.id){
-                                                Mediator.publish('workflow:unCheckDept', staff[k]);
-                                            }
-                                        }
-                                    }
-                                },
-                                treeType:'MULTI_SELECT',
-                                isSearch: true,
-                                withButtons:true
-                                });
-                            treeComp2.render($('#treeMulti'));
-                        });
-                    })
-                }else{
-                    (async function () {
-                        return workflowService.getStuffInfo({url: '/get_department_tree/'});
-                    })().then(res=>{
-                        tree=res.data.department_tree;
-                        staff=res.data.department2user;
-                        function recur(data) {
-                            for (let item of data){
-                                item.nodes=item.children;
-                                if(item.children.length!==0){
-                                    recur(item.children);
-                                }
-                            }
-                        }
-                        recur(tree);
-                        var treeComp2 = new TreeView(tree,{
-                            callback: function (event,selectedNode) {
-                                if(event==='select'){
-                                    for(var k in staff){
-                                        if(k==selectedNode.id){
-                                            Mediator.publish('workflow:checkDept', staff[k]);
-                                        }
-                                    }
-                                }else{
-                                    for(var k in staff){
-                                        if(k==selectedNode.id){
-                                            Mediator.publish('workflow:unCheckDept', staff[k]);
-                                        }
-                                    }
-                                }
-                            },
-                            treeType:'MULTI_SELECT',
-                            isSearch: true,
-                            withButtons:true
-                            });
-                        treeComp2.render($('#treeMulti'));
-                    });
-                }
-            
                 (async function () {
                     return workflowService.getWorkflowInfo({url: '/get_all_users/'});
                 })().then(users => {
