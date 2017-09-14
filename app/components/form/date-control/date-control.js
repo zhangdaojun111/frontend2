0889a5acbfaa73728e8f2c2374d4e9c331e6d4f4
lichen <xy1ona@163.com>
2017-09-14 10:07:09 +0800

    []

--- app/components/form/date-control/date-control.js
+++ app/components/form/date-control/date-control.js
@@ -64,9 +64,14 @@ let config = {
     afterRender() {
         let _this = this;
         this.el.find('.ui-width').css('width', this.data.width);
-        if(!this.data.isCalendar && this.data.history){
+
+        if(! this.data.isCalendar && !this.data.isAgGrid && this.data.history){
             this.el.find('.ui-history').css('visibility','visible');
         }
+        if(this.data.isCalendar || this.data.isAgGrid) {
+            this.el.find('.ui-history').css('visibility','hidden');
+        }
+        
         if (this.data.is_view) {
             this.el.find('.ui-width').attr('disabled', true);
             this.el.find('.component-date-control').css('pointer-events','none');
