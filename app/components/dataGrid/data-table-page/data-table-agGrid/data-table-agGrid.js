0d184a8f163079041f31cb8964576d89ff4b84c0
yangxiaochuan <yangxiaochuan@thinktrader.net>
2017-09-15 09:34:44 +0800

    [bugfix]排序加loading

--- app/components/dataGrid/data-table-page/data-table-agGrid/data-table-agGrid.js
+++ app/components/dataGrid/data-table-page/data-table-agGrid/data-table-agGrid.js
@@ -2474,7 +2474,13 @@ let config = {
         //触发排序事件
         onSortChanged: function ($event) {
             if( this.data.frontendSort ){
+                this.data.showTabs( 0 );
+                this.showLoading();
                 this.agGrid.actions.refreshView();
+                setTimeout( ()=>{
+                    this.hideLoading();
+                    this.data.showTabs( 1 );
+                },200 )
             }
             if( this.data.viewMode == 'viewFromCorrespondence' || this.data.viewMode == 'editFromCorrespondence' || this.data.frontendSort ){
                 return;
