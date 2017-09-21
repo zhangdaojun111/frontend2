25ec76f592ee7a54e2de1fe8523c16d47d5c1499
ZY <510980866@qq.com>
2017-09-21 17:00:10 +0800

    password modify

--- app/components/util/passwordInput/password-input.js
+++ app/components/util/passwordInput/password-input.js
@@ -10,17 +10,17 @@ import 'jquery-ui/ui/widgets/dialog.js';
 import template from './password-input.html';
 import './password-input.scss';
 
-
 let config = {
     template:template,
     data:{
         password_value:'',
         isLegal:false,
         isCapsLockOpen:false,
+        checkChar:true,
     },
     actions:{
         initPswInput:function () {
-            this.el.find('.password-title').html(this.data.title + "：");
+            this.el.find('.password-title').html(this.data.title);
             this.el.find('.password-rule').html(this.data.rule);
         },
         /**
@@ -31,9 +31,9 @@ let config = {
             let e = event;
             let $capitalTip = this.el.find('.caps-lock-tips');
             let keyCode = e.keyCode;
-
             let isShift = e.shiftKey || (keyCode === 16 ) || false;
-            if(((keyCode >= 65 && keyCode <= 90 ) && !isShift) || (( keyCode >= 97 && keyCode <= 122 ) && isShift)){
+
+            if((( keyCode >= 65 && keyCode <= 90 ) && !isShift ) || (( keyCode >= 97 && keyCode <= 122 ) && isShift )){
                 $capitalTip.show();
                 this.actions.setBubblePopup();
                 this.data.isCapsLockOpen = true;
@@ -42,8 +42,11 @@ let config = {
                 $capitalTip.hide();
             }
         },
+        /**
+         * 通过监听keyup监听capslock键，切换大小写模式
+         * @param event
+         */
         listenKeyUp:function(event) {
-            // 如果是大小写锁定键，直接切换状态
             let e = event;
             let $capitalTip = this.el.find('.caps-lock-tips');
             let keyCode = e.keyCode;
@@ -72,11 +75,14 @@ let config = {
                 return;
             }
             //非法字符检测
-            if(this.actions.checkLegalChar(this.data.password_value) === false){
-                this.el.find('.input-password-warning').html('密码中不能含有特殊字符，请修改');
-                this.data.isLegal = false;
-                return;
+            if(this.data.checkChar === true){
+                if(this.actions.checkLegalChar(this.data.password_value) === false){
+                    this.el.find('.input-password-warning').html('密码中不能含有特殊字符，请修改');
+                    this.data.isLegal = false;
+                    return;
+                }
             }
+
             if(this.actions.checkPwLength(this.data.password_value) === false){
                 this.el.find('.input-password-warning').html('密码长度必须为6-16位，请修改');
                 this.data.isLegal = false;
@@ -98,12 +104,18 @@ let config = {
          * @param pw
          */
         checkPwLength:function (pw) {
-            let reg = /^[a-z0-9]{6,16}$/i;
-            return reg.test(pw);
+            let length = pw.length;
+            return (length >=6 && length <= 16 )
         },
+        /**
+         * input获取焦点时隐藏密码不合法的提醒
+         */
         removeTips:function () {
             this.el.find('.input-password-warning').html('');
         },
+        /**
+         * 切换到用户信息显示页面时，重置所有input框的内容
+         */
         resetInput:function () {
             this.password_value = '';
             this.isLegal = false;
@@ -111,9 +123,6 @@ let config = {
             this.el.find('.input-password-warning').html('');
             this.el.find('.caps-lock-tips').hide();
         },
-        setBubblePopup:function () {
-            
-        }
     },
     binds:[
         {
@@ -139,12 +148,10 @@ let config = {
         },{
             event:'blur',
             selector:'.set-password-input',
-            callback:function (target,event) {
+            callback:function (target,event) {          //input框失去焦点时，设置密码值以及检测密码是否合法
                 this.actions.checkPswLegal();
             }
         },
-
-
     ],
     afterRender:function () {
         this.actions.initPswInput();
