f4113edcc124820cd5fd4e2d132e1760ae210725
zhaohaoran <zhaohaoran44@126.com>
2017-09-22 18:03:58 +0800

    [bugfix]显示勾选项

--- app/components/dataGrid/data-table-page/data-table-agGrid/data-table-agGrid.js
+++ app/components/dataGrid/data-table-page/data-table-agGrid/data-table-agGrid.js
@@ -37,6 +37,7 @@ import QuillAlert from "../../../form/quill-alert/quill-alert";
 let config = {
     template: template,
     data: {
+        fristGet:true,
         tableId: '',
         formId: '',
         tableType: '',
@@ -1139,6 +1140,10 @@ let config = {
             }
             Promise.all(post_arr).then((res)=> {
                 this.actions.setGridData( res );
+                if(this.data.fristGet){
+                    this.actions.checkCorrespondence();
+                    this.data.fristGet = false;
+                }
                 if(refresh){
                     msgBox.showTips( '数据刷新成功。' )
                 }
