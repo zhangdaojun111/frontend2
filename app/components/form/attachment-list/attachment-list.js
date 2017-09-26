e3c4edf7fd522c5f422f56be42386286ea2702db
Jenny Yan <yunxuan.yan@gmail.com>
2017-09-27 12:58:44 +1300

    [feature]图片预览中的下载、放大缩小、显示比例、回到初始化状态，以及旋转的反应非常迟钝问题

--- app/components/form/attachment-list/attachment-list.js
+++ app/components/form/attachment-list/attachment-list.js
@@ -82,6 +82,11 @@ let css = `
         left: 0;
         top: 0;
     }
+    .closeImg{
+        position: absolute;
+        right:20px;
+        top:20px;
+    }
     .mask-div{
         position: absolute;
         z-index: 2;
@@ -99,12 +104,12 @@ let css = `
         z-index: 10;
         max-height:80%;
     }
-    .rotate-button{
+    .operator-buttons{
         position: absolute;
         transform: translate(50%,-50%);
         right: 50%;
         bottom: 0;
-        width: 300px;
+        width: 350px;
         height: 50px;
         z-index: 10;
         padding:0 20px;
@@ -114,8 +119,12 @@ let css = `
         align-items:center;
         justify-content: center;
     }
-    .rotate-button li{
+    .operator-buttons li{
         padding:5px 15px;
+    }
+    .scale {
+        font-size:20px;
+        color:white;
     }
     `;
 let AttachmentList = {
@@ -133,109 +142,162 @@ let AttachmentList = {
         fileGroup: [],
         preview_file: ["gif", "jpg", "jpeg", "png", "txt", "pdf", "lua", "sql", "rm", "rmvb", "wmv", "mp4", "3gp", "mkv", "avi"],
     },
+    binds: [
+        {
+            //图片弹窗预览
+            event:'click',
+            selector:'.pre',
+            callback:function (event) {
+                this.el.find('.my-mask').show();
+                this.data.rotateNo = 0;
+                this.data.imgScale = 1;
+                this.el.find('.img-pre').css("height", $(window).height() * 0.7 + 'px');
+                this.el.find(".img-pre").css("transform", "translate(-50%,-50%) rotate(" + this.data.rotateNo + "deg) scale(" + this.data.imgScale + "," + this.data.imgScale + ")");
+                this.el.find('.img-pre').get(0).src = "/download_attachment/?file_id=" + event.dataset.id + "&download=0&dinput_type=" + this.data.dinput_type;
+                $(document).on("mousewheel DOMMouseScroll", (e) => {
+                    let delta = (e.originalEvent['wheelDelta'] && (e.originalEvent['wheelDelta'] > 0 ? 1 : -1)) ||
+                        (e.originalEvent['detail'] && (e.originalEvent['detail'] > 0 ? -1 : 1));
+                    if (delta > 0) {
+                        this.data.imgScale += 0.1;
+                        this.el.find(".img-pre").css("transform", "translate(-50%,-50%) rotate(" + this.data.rotateNo + "deg) scale(" + this.data.imgScale + ")");
+                    } else if (delta < 0) {
+                        this.imgScale -= 0.1;
+                        this.el.find(".img-pre").css("transform", "translate(-50%,-50%) rotate(" + this.data.rotateNo + "deg) scale(" + this.data.imgScale + ")");
+                    }
+                });
+                this.el.find(".save").attr('id',event.dataset.id);
+            }
+        }, {
+            //删除
+            event: 'click',
+            selector: '.del',
+            callback: function (event) {
+                let _this = this;
+                let fielIds = $(event.target).attr('id');
+                HTTP.post('delete_attachment', {
+                    file_ids: JSON.stringify([fielIds]),
+                    dinput_type: this.data.dinput_type
+                }).then(res => {
+                    _this.data.list = res["rows"];
+                    for (let i = 0, len = _this.data.list.length; i < len; i++) {
+                        if (_this.data.list[i]["file_id"] == fielIds) {
+                            _this.data.list.splice(i, 1);
+                            break;
+                        }
+                    }
+                    // this.wfService.sendDelFile(fileId);
+                    // this.wfService.sendDelQQimg(fileId);
+                    // this.emitDelAttachmentIds.emit(fileId);
+                    _this.reload();
+                });
+                HTTP.flush();
+            }
+        }, {
+            event: 'click',
+            selector: '.mask-div,.closeImg',
+            callback: function () {
+                this.el.find('.my-mask').hide();
+                this.data.dragStart = false;
+                $(document).off("mousewheel DOMMouseScroll");
+            }
+        }, {
+            event: 'mouseup',
+            selector: '.img-pre',
+            callback: function () {
+                this.data.dragStart = false;
+            }
+        }, {
+            event: 'mousemove',
+            selector: '.img-pre',
+            callback: function ($event) {
+                //是否拖拽
+                if (!this.data.dragStart) {
+                    return;
+                }
+
+                //初始值调用 计算位移偏差值
+                let disX = $event.clientX - this.data.dragStartX;
+                let disY = $event.clientY - this.data.dragStartY;
+
+                //计算图片相对位置
+                let goX = this.data.imgStartX + disX + $(".img-pre").width() / 2;
+                let goY = this.data.imgStartY + disY + $(".img-pre").height() / 2;
+
+                $($event.target).css({'top': goY + 'px', 'left': goX + 'px'});
+            }
+        }, {
+            event: 'mousedown',
+            selector: '.img-pre',
+            callback: function ($event) {
+                $event.preventDefault && $event.preventDefault();//这个很重要
+                //dragStart==可拖拽标识
+                this.data.dragStart = true;
+
+                //初始值记录
+                this.data.dragStartX = $event.clientX;
+                this.data.dragStartY = $event.clientY;
+                this.data.imgStartX = this.el.find('.img-pre').position().left;
+                this.data.imgStartY = this.el.find('.img-pre').position().top;
+            }
+        }, {
+            event:'click',
+            selector: '.save',
+            callback:function (event) {
+                this.el.find('.download-url').attr('href','/download_attachment/?file_id='+event.id+'&download=1&dinput_type={{'+this.data.dinput_type+'}}');
+            }
+        }, {
+            event: 'click',
+            selector: '.zoom-out',
+            callback: function () {
+                this.data.imgScale -= 0.1;
+                this.actions._updatePreview();
+            }
+        }, {
+            event: 'click',
+            selector: '.zoom-in',
+            callback: function () {
+                this.data.imgScale += 0.1;
+                this.actions._updatePreview();
+            }
+        }, {
+            event: 'click',
+            selector: '.rotate',
+            callback: function (event) {
+                this.data.rotateNo -= 90;
+                this.data.imgScale = 1;
+                this.el.find(".img-pre").css({
+                    "transform": "translate(-50%,-50%) rotate(" + this.data.rotateNo + "deg)",
+                    "top": "50%",
+                    "left": "50%"
+                });
+                this.actions._uploadScale();
+            }
+        }, {
+            event:'click',
+            selector: '.resize',
+            callback:function () {
+                this.data.imgScale = 1;
+                this.data.rotateNo = 0;
+                this.actions._updatePreview();
+            }
+        }
+    ],
     actions: {
         //获取文件名后缀
         getFileExtension(filename) {
             return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+        },
+        _uploadScale() {
+            this.el.find('.scale').text(Math.floor(this.data.imgScale*100)+"%");
+        },
+        _updatePreview() {
+            this.el.find(".img-pre").css('transform','translate(-50%,-50%) rotate(' + this.data.rotateNo + 'deg) scale(' + this.data.imgScale + ')');
+            this.actions._uploadScale();
         }
     },
     afterRender() {
-        //许多操作已暂时弃用 代码留着 有备无患
         this.data.style = $("<style></style>").text(this.data.css).appendTo($("head"));
-        let _this = this;
-        //图片弹窗预览
-        this.el.on('click', '.pre', function (event) {
-            _this.el.find('.my-mask').show();
-            _this.data.rotateNo = 0;
-            _this.data.imgScale = 1;
-            _this.el.find('.img-pre').css("height", $(window).height() * 0.7 + 'px');
-            _this.el.find(".img-pre").css("transform", "translate(-50%,-50%) rotate(" + _this.data.rotateNo + "deg) scale(" + _this.data.imgScale + "," + _this.data.imgScale + ")");
-            let myLocated = location.href.split('#');
-            // _this.el.find('.img-pre').get(0).src=myLocated[0]+"download_attachment/?file_id="+$(event.target).data("id")+"&download=0&dinput_type="+_this.data.dinput_type;
-            _this.el.find('.img-pre').get(0).src = "/download_attachment/?file_id=" + $(event.target).data("id") + "&download=0&dinput_type=" + _this.data.dinput_type;
-            $(document).on("mousewheel DOMMouseScroll", (e) => {
-                let delta = (e.originalEvent['wheelDelta'] && (e.originalEvent['wheelDelta'] > 0 ? 1 : -1)) ||
-                    (e.originalEvent['detail'] && (e.originalEvent['detail'] > 0 ? -1 : 1));
-                if (delta > 0) {
-                    _this.data.imgScale += 0.1;
-                    _this.el.find(".img-pre").css("transform", "translate(-50%,-50%) rotate(" + _this.data.rotateNo + "deg) scale(" + _this.data.imgScale + ")");
-                } else if (delta < 0) {
-                    this.imgScale -= 0.1;
-                    _this.el.find(".img-pre").css("transform", "translate(-50%,-50%) rotate(" + _this.data.rotateNo + "deg) scale(" + _this.data.imgScale + ")");
-                }
-            });
-        });
-        //删除
-        this.el.on('click', '.del', function (event) {
-            let _this = this;
-            let fielIds = $(event.target).attr('id');
-            HTTP.post('delete_attachment', {
-                file_ids: JSON.stringify([fielIds]),
-                dinput_type: this.data.dinput_type
-            }).then(res => {
-                _this.data.list = res["rows"];
-                for (let i = 0, len = _this.data.list.length; i < len; i++) {
-                    if (_this.data.list[i]["file_id"] == fielIds) {
-                        _this.data.list.splice(i, 1);
-                        break;
-                    }
-                }
-                // this.wfService.sendDelFile(fileId);
-                // this.wfService.sendDelQQimg(fileId);
-                // this.emitDelAttachmentIds.emit(fileId);
-                _this.reload();
-            })
-            HTTP.flush();
-        });
-
-        this.el.on('click', '.mask-div,.closeImg', function () {
-            _this.el.find('.my-mask').hide();
-            _this.data.dragStart = false;
-            $(document).off("mousewheel DOMMouseScroll");
-        })
-        this.el.on('mouseup', '.img-pre', function () {
-            _this.data.dragStart = false;
-        }).on('mousemove', '.img-pre', function ($event) {
-            //是否拖拽
-            if (!_this.data.dragStart) {
-                return;
-            }
-
-            //初始值调用 计算位移偏差值
-            let disX = $event.clientX - _this.data.dragStartX;
-            let disY = $event.clientY - _this.data.dragStartY;
-
-            //计算图片相对位置
-            let goX = _this.data.imgStartX + disX + $(".img-pre").width() / 2;
-            let goY = _this.data.imgStartY + disY + $(".img-pre").height() / 2;
-
-            $($event.target).css({'top': goY + 'px', 'left': goX + 'px'});
-        }).on('mousedown', '.img-pre', function ($event) {
-            $event.preventDefault && $event.preventDefault();//这个很重要
-            //dragStart==可拖拽标识
-            _this.data.dragStart = true;
-
-            //初始值记录
-            _this.data.dragStartX = $event.clientX;
-            _this.data.dragStartY = $event.clientY;
-            _this.data.imgStartX = _this.el.find('.img-pre').position().left;
-            _this.data.imgStartY = _this.el.find('.img-pre').position().top;
-        })
-        this.el.on('click','.zoom-out',function () {
-            _this.data.imgScale -= 0.1;
-            _this.el.find(".img-pre").css("transform", "translate(-50%,-50%) rotate(" + _this.data.rotateNo + "deg) scale(" + _this.data.imgScale + ")");
-        }).on('click', '.zoom-in', function () {
-            _this.data.imgScale += 0.1;
-            _this.el.find(".img-pre").css("transform", "translate(-50%,-50%) rotate(" + _this.data.rotateNo + "deg) scale(" + _this.data.imgScale + ")");
-        }).on('click', '.rotate', function (event) {
-            _this.data.rotateNo -= 90;
-            _this.data.imgScale = 1;
-            $(event.target).parent().parent().find(".img-pre").css({
-                "transform": "translate(-50%,-50%) rotate(" + _this.data.rotateNo + "deg) scale(" + _this.data.imgScale + ")",
-                "top": "50%",
-                "left": "50%"
-            });
-        }).on('click','')
+        this.actions._uploadScale();
     },
     beforeDestory() {
         this.data.style.remove();
