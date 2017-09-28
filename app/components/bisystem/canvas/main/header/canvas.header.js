e89a3f0fea87f18ba0836f8c95e9999a6322a738
luoweiqiang <15681276901@163.com>
2017-09-28 10:43:55 +0800

    canvas.header.menu导航切换

--- app/components/bisystem/canvas/main/header/canvas.header.js
+++ app/components/bisystem/canvas/main/header/canvas.header.js
@@ -43,24 +43,63 @@ let config = {
             this.trigger('onAddCell', layout);
         },
 
-        // /**
-        //  * 当前选中的视图
-        //  * @param id 当前视图的id
-        //  * @returns {*}
-        //  */
-        // canSaveViews(viewId) {
-        //     let biViews = window.config.bi_views;
-        //     let isSelf = _.result(_.find(biViews,{'id':parseInt(viewId)}),'self');
-        //     if(isSelf==1){
-        //         this.el.find('.canSaveView').show();
-        //     }else{
-        //         this.el.find('.canSaveView').hide();
-        //     }
-        // }
+        /**
+         * 当前选中的视图
+         * @param id 当前视图的id
+         * @returns {*}
+         */
+        canSaveViews(viewId) {
+            let biViews = window.config.bi_views;
+            let isSelf = _.result(_.find(biViews,{'id':parseInt(viewId)}),'self');
+            if(isSelf==1){
+                this.el.find('.canSaveView').show();
+            }else{
+                this.el.find('.canSaveView').hide();
+            }
+        },
+        /**
+         * 初始化加载时隐藏的更多目录框
+         */
+        hideMoreMenu(){
+            let childWidth = 0;
+            let parentWidth = this.el.find('.nav-tabs').width();
+            this.el.find('.nav-tabs-select').css('left',parentWidth);
+            this.el.find('.child-menu').css('left',parentWidth);
+            this.el.find('.nav-tabs a').each((index,val)=>{
+                childWidth += $(val).outerWidth();
+                if(childWidth > parentWidth){
+                    $(val).parent().appendTo(this.el.find('.child-menu'));
+                }
+            });
+        },
+        /**
+         *当窗口大小改变时 更多目录框的显示随之改变
+         */
+        windowChange(){
+            let childWidth = 0;
+            let parentWidth = this.el.find('.nav-tabs').width();
+            this.el.find('.nav-tabs-select').css('left',parentWidth);
+            this.el.find('.child-menu').css('left',parentWidth);
+            this.el.find('.nav-tabs div').each((index,val)=>{
+                childWidth += $(val).width();
+                if(childWidth > parentWidth){
+                    if(this.el.find('.child-menu div').length!==0){
+                        this.el.find('.child-menu div:first-child').before($(val));
+                    }else {
+                        $(val).appendTo(this.el.find('.child-menu'));
+                    }
+                }
+            });
+            this.el.find('.child-menu div').each((index,val)=>{
+                childWidth +=$(val).width();
+                if(childWidth < parentWidth){
+                    $(val).appendTo(this.el.find('.nav-tabs'));
+                }
+            })
+        }
     },
     binds: [
-        //保存画布块
-        {
+        { //保存画布块
             event: 'click',
             selector: '.views-btn-group .view-save-btn',
             callback: function (context, event) {
@@ -86,12 +125,19 @@ let config = {
                 formData.append('file',$(context)[0].files[0]);
                 canvasCellService.importData(formData).then((res)=>{
                     if (res['success'] === 1) {
-                        // msgbox.alert('上传成功');
                         location.reload();
                     } else {
                         msgbox.alert(res['error']);
                     }
                 })
+            }
+        },
+        { //点击显示更多目录
+            event:'click',
+            selector:'.nav-tabs-select',
+            callback: function (context,event) {
+                this.el.find('.child-menu').show();
+                event.stopPropagation();
             }
         },
     ],
@@ -100,15 +146,39 @@ let config = {
         //新窗口隐藏新窗口图标
         if(window === window.parent){
             this.el.find('.new-window').hide();
-        };
+        }
 
         this.data.views = window.config.bi_views;
         // 渲染header视图列表
         this.data.views.forEach(viewData => {
-            let menu = new CanvasHeaderMenuComponent(viewData);
+            let menu = new CanvasHeaderMenuComponent(viewData,{
+                onClearActive:()=>{
+                    console.log('do a test');
+
+                }
+            });
             this.append(menu, this.el.find('.nav-tabs'));
             this.data.menus[viewData.id] = menu;
+        });
+
+        //第一次渲染之后隐藏多的目录
+        this.actions.hideMoreMenu();
+
+    },
+    firstAfterRender(){
+        //当窗口大小改变时 更多目录框的显示随之改变
+        $(window).resize(()=> {
+            this.actions.windowChange();
+        });
+
+        //点击更多目录框外 隐藏显示框
+        $(document.body).bind('click.menu',()=>{
+           this.el.find('.child-menu').hide();
         })
+    },
+    beforeDestory(){
+        //当destory时销毁全局document.body click事件
+        $(document.body).off('click.menu');
     }
 };
 export class CanvasHeaderComponent extends Component {
--- app/components/bisystem/canvas/main/header/menu/canvas.header.menu.js
+++ app/components/bisystem/canvas/main/header/menu/canvas.header.menu.js
@@ -20,6 +20,8 @@ let config = {
             brothers.forEach((item) => {
                 item.actions.blur();
             });
+            //发送通知给canvas.header组件 删除隐藏的更多目录框的选中状态
+            this.trigger('onClearActive');
             this.data.isCurrent = true;
         },
         blur(){
@@ -33,15 +35,16 @@ let config = {
         {
             event: 'click',
             selector: '',
-            callback: function () {
+            callback: function (context,event) {
                 this.actions.focus();
+                return false;
             }
         }
     ],
     afterRender() {
-        if (this.data.isCurrent) {
-            this.el.find('a').addClass('active');
-        }
+        // if (this.data.isCurrent) {
+        //     this.el.find('a').addClass('active');
+        // }
     }
 };
 
