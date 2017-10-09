1f7ef43beca57cd3b157d873573b7ecd2d387071
yangxiaochuan <yangxiaochuan@thinktrader.net>
2017-10-09 15:16:49 +0800

    [bugfix]他人登录拼音BUG

--- app/components/main/login-by-other/login-by-other.js
+++ app/components/main/login-by-other/login-by-other.js
@@ -31,7 +31,11 @@ let config = {
                     this.data.userData = result.rows;
                     for (let row of this.data.userData){
                         if(row.name && row.name.trim() !== '' && row.id && row.id.trim() !== ''){
-                            row.py = row.f7_p.join(',');
+                            if( row.f7_p ){
+                                row.py = row.f7_p.join(',');
+                            }else {
+                                row.py = '';
+                            }
                             tempData.push(row);
                         }
                     }
