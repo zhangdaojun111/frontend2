ffae041c87e60c5a785bfc78e9d6de614f68ab7b
yincaihua <249410592@qq.com>
2017-10-16 10:49:34 +0800

    更新master

---
+++ app/entrys/popup/customDataGrid.js
@@ -0,0 +1,61 @@
+import workflowPage from '../../components/customPage/workflow-page/workflow-page.js'
+import myWorkflow from '../../components/customPage/my-workflow/my-workflow.js'
+import myOperation from '../../components/customPage/my-operation/my-operation.js'
+import department from '../../components/customPage/department/department'
+import personnel from '../../components/customPage/personnel/personnel'
+import departmentDiary from '../../components/customPage/department-diary/department-diary'
+$(document).ready(function(){
+    let isNewWindow = window.location.href.indexOf( 'isNewWindow=true' ) == -1 ? false:true;
+    let tableName = window.config.tableName || 'ERDS';
+    let ts_name = window.config.ts_name;
+    let com = null;
+    let json = {};
+    let workflowPages = ['approve-workflow','approving-workflow','finished-workflow'];
+    if( workflowPages.indexOf( ts_name ) != -1 ){
+        json = {
+            tableId: ts_name,
+            isNewWindow: isNewWindow
+        }
+        com = new workflowPage(json);
+    }
+    if( ts_name == 'my-workflow' ){
+        json = {
+            tableId: ts_name,
+            isNewWindow: isNewWindow
+        }
+        com = new myWorkflow(json);
+    }
+    if( ts_name == 'my-operations'){
+        tableName = '我的操作';
+        json = {
+            tableId: ts_name,
+            isNewWindow: isNewWindow
+        }
+        com = new myOperation(json);
+    }
+    if( ts_name == 'department-information' ){
+        tableName = '部门信息';
+        json = {
+            tableId: window.config.table_id,
+            isNewWindow: isNewWindow
+        }
+        com = new department(json);
+    }
+    if( ts_name == 'personal-information' ){
+        tableName = '人员信息';
+        json = {
+            tableId: window.config.table_id,
+            isNewWindow: isNewWindow
+        }
+        com = new personnel(json);
+    }
+    if( ts_name == 'department-daily' ){
+        tableName = '部门日报';
+        json = {
+            tableName: '部门日报',
+            isNewWindow: isNewWindow
+        }
+        com = new departmentDiary(json);
+    }
+    com.render($('#customDataGrid'));
+})
