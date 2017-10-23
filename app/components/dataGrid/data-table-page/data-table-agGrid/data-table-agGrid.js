3f452b5945fe8312d8ab5ced284310f97faf949d
zj <somiarzj@hotmail.com>
2017-10-23 09:42:21 +0800

    bugfix

--- app/components/dataGrid/data-table-page/data-table-agGrid/data-table-agGrid.js
+++ app/components/dataGrid/data-table-page/data-table-agGrid/data-table-agGrid.js
@@ -3237,10 +3237,8 @@ let config = {
         },
         //打开局部的弹窗
         openSelfIframe: function ( url,title,w,h ) {
-            w = document.documentElement.clientWidth*0.8;
-            console.log(w);
+            w = window.screen.width*0.8;
             h = window.screen.height*0.6;
-            console.log(window.screen.width, window.screen.height);
             PMAPI.openDialogToSelfByIframe( url,{
                     width: w || 1400,
                     height: h || 800,
