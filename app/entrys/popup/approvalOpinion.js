58c18f18d4142b98acd608e8baf790fc63fb0048
zj <somiarzj@hotmail.com>
2017-10-25 17:02:28 +0800

    bugfix

---
+++ app/entrys/popup/approvalOpinion.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zj on 2017/10/25.
+ */
+import 'jquery-ui/ui/widgets/button.js';
+import 'jquery-ui/ui/widgets/dialog.js';
+
+// import approvalOpinion from '../../components/workflow/approval-opinion/approval-opinion';
+// let approvalOpinion = new approvalOpinion();
+//
+// approvalOpinion.render($('#calendarExport'));
