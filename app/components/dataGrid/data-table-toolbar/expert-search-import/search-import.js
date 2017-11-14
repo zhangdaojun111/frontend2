1c0a6ac485a8d7c2274bbd5bc31bacbb0bef013e
zhaohaoran <zhaohaoran44@126.com>
2017-11-14 15:12:21 +0800

    [bugfix]

--- app/components/dataGrid/data-table-toolbar/expert-search-import/search-import.js
+++ app/components/dataGrid/data-table-toolbar/expert-search-import/search-import.js
@@ -120,38 +120,3 @@ class searchImport extends Component {
     }
 }
 export default searchImport
-
-
-
-
-
-
-
-// let obj = {
-//     selectMode:'single',
-//     file_filter:'.ini'
-// }
-// let uploader = new Uploader(obj);
-// let fileData;
-// export const searchImport = {
-//     import: function (key,id,choice){
-//         uploader.addFile(key).then(res=> {
-//             fileData = res;
-//         })
-//         let json = {
-//             file:'upload_file',
-//             table_id: id,
-//             override: choice
-//         }
-//         uploader.appendData( json );
-//         let url = `/import_queryparams/?table_id=${id}&override=${choice}`;
-//         let toolbox = {
-//             update:function () {},
-//             finish:function (res) {},
-//             showError:function () {}
-//         };
-//         uploader.upload(url,{},toolbox.update(),(res)=>{
-//             console.log(res)
-//         })
-//     }
-// }
