d96b998ca93d00abe91d906dd26e90284a928c9d
lichen <xy1ona@163.com>
2017-11-14 19:17:21 +0800

    []

--- app/components/workflow/approval-opinion/approval-opinion.js
+++ app/components/workflow/approval-opinion/approval-opinion.js
@@ -4,13 +4,15 @@ import template from './approval-opinion.html'
 import './approval-opinion.scss';
 import {PMAPI} from '../../../lib/postmsg'
 import AttachmentControl from "../../form/attachment-control/attachment-control";
-
+import msgbox from '../../../lib/msgbox';
 
 let config = {
     template: template,
     data: {
         // css: css.replace(/(\n)/g, ''),
         fileList: [],
+        queue:'',
+
     },
     binds:[
         {
@@ -18,16 +20,25 @@ let config = {
             selector: '.J_sure',
             callback: function (event) {
                 this.actions.determine();
-                PMAPI.sendToParent({
-                    type: '1',
-                    key: window.config.key,
-                    data: {
-                        determine:true,
-                        key: this.key,
-                        comment: this.data.comment,
-                        attachment: this.data.fileList
-                    }
-                })
+                let uploadingState
+                for(let k in this.data.queue) {
+                    uploadingState = this.data.queue[k]['uploadingState'];
+                }
+                if(uploadingState != ''){
+                    PMAPI.sendToParent({
+                        type: '1',
+                        key: window.config.key,
+                        data: {
+                            determine:true,
+                            key: this.key,
+                            comment: this.data.comment,
+                            attachment: this.data.fileList
+                        }
+                    })
+                }else{
+                    msgbox.alert('上传未完成');
+                }
+
             }
         },
         {
@@ -56,6 +67,7 @@ let config = {
         };
         let changeValue = (res) => {
             this.data.fileList = res.value;
+            this.data.queue = res.queue;
         };
         let attachmentControl = new AttachmentControl(json, {changeValue: changeValue});
         this.append(attachmentControl, this.el.find('.workflow-attachment-box'));
