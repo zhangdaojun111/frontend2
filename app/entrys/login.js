fc028ec6dc7b6dc4fcc23350d0f57fb569bf2857
XiongXiaotao <xxtao110@163.com>
2017-11-29 18:24:10 +0800

    登录页面新增背景可配置

--- app/entrys/login.js
+++ app/entrys/login.js
@@ -399,6 +399,7 @@ function resetLoginBoxInfo(result) {
 }
 
 function resetBackground(result) {
+    console.log(result);
     let backgroundSrc = result.client_login_params && result.client_login_params.back_ground.login_back_ground;
     let coverSrc = result.client_login_params && result.client_login_params.back_ground.login_cover_on;
     if (backgroundSrc) {
