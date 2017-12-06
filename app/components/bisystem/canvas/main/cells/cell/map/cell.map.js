f459aedd6a885513a4db71da2476fee0979f6c55
luoweiqiang <15681276901@163.com>
2017-12-06 11:34:06 +0800

    自定义设置精度

--- app/components/bisystem/canvas/main/cells/cell/map/cell.map.js
+++ app/components/bisystem/canvas/main/cells/cell/map/cell.map.js
@@ -15,7 +15,6 @@ let config = {
     actions: {
         echartsInit() {
             this.customAccuracy(this.data.cellChart.chart);
-            console.log(this.data.cellChart.chart);
             let echartsService = new EchartsService(this.data);
             this.myChart = echartsService.myChart;
             let that = this;
