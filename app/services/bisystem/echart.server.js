d74e97e48443ffcf50aa43ad6f3550f80f38938a
ZY <510980866@qq.com>
2017-12-07 10:59:55 +0800

    modify src

--- app/services/bisystem/echart.server.js
+++ app/services/bisystem/echart.server.js
@@ -194,6 +194,7 @@ export class EchartsService {
             let str = cellChart.chart.data.xAxis[0];
             let len = str.length;
             linebarOption['grid']['right'] = Math.min(Math.max(len*1000/(cellWidth^3),15),40);
+
             // linebarOption['yAxis'][0]['interval'] = Math.abs(firstMax / splitNumber);
         } else if (cellOption.double === 1) {
             // 双y轴 如果有一个y轴小于0
