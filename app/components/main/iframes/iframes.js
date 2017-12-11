ee770c5cbb77a104ee496662121983a0666de445
xxt <xxtao110@163.com>
2017-12-11 18:25:57 +0800

    视图配置为空的bug

--- app/components/main/iframes/iframes.js
+++ app/components/main/iframes/iframes.js
@@ -492,7 +492,7 @@ let IframeComponent = Component.extend({
                 if (result[3].succ === 1) {
                     homeConfig = result[3];
                     if (!homeConfig.data.length) {
-                        msgbox.showTips("服务器暂不支持首页，请及时更新");
+                        // msgbox.showTips("服务器暂不支持首页，请及时更新");
                         homeConfig.data = "000";
                     }
                     if (homeConfig.data.substring(homeConfig.data.length - 1) !== "0" && homeConfig.data.substring(homeConfig.data.length - 1) !== "1") {
