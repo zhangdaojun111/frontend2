b3470c3d238470f94a52a54160be00f3836d7357
lichen <xy1ona@163.com>
2017-12-13 15:58:43 +0800

    []

--- app/components/form/base-form/base-form.js
+++ app/components/form/base-form/base-form.js
@@ -2263,7 +2263,7 @@ let config = {
 						this.actions.createRadioControl(data,key,single,actions);
 						break;
 					case 'Input':
-						this.actions.createInputControl(data,key,single,'Input',actions)
+						this.actions.createInputControl(data,key,single,actions);
 						break;
 					case 'Textarea':
 						this.actions.createTextareaControl(data,key,single,actions);
@@ -2442,7 +2442,6 @@ let config = {
 			if (this.data.btnType != 'none') {
 				this.actions.addBtn();
 			}
-			debugger
             if(window.top.miniFormVal && !this.data.data['real_id']['value']){
 				let miniFormVal =  window.top.miniFormVal[this.data.data['table_id']['value']]
 				for(let k in miniFormVal){
@@ -2471,34 +2470,5 @@ let config = {
 	}
 }
 
-// class BaseForm extends Component {
-// 	// constructor(formData, newConfig) {
-// 	// 	config.template = formData.template;
-// 	// 	//存父子表关系
-// 	// 	if (!window.top.frontendRelation) {
-// 	// 		window.top.frontendRelation = {};
-// 	// 	}
-// 	// 	if (!window.top.frontendParentNewData) {
-// 	// 		window.top.frontendParentNewData = {};
-// 	// 	}
-// 	// 	if (!window.top.isSonGridDataNeedParentTepmId) {
-// 	// 		window.top.isSonGridDataNeedParentTepmId = '';
-// 	// 	}
-// 	// 	if (!window.top.idsInChildTableToParent) {
-// 	// 		window.top.idsInChildTableToParent = {};
-// 	// 	}
-// 	// 	if (!window.top.frontendParentFormValue) {
-// 	// 		window.top.frontendParentFormValue = {};
-// 	// 	}
-// 	// 	window.top.frontendRelation[formData.data.tableId] = formData.data["frontend_cal_parent_2_child"];
-// 	// 	//存父表的newData
-// 	// 	window.top.frontendParentNewData[formData.data.tableId] = _.defaultsDeep({},formData.data.data);
-// 	// 	window.top.isSonGridDataNeedParentTepmId = formData.data.data['temp_id'] && formData.data.data['temp_id']['value']?formData.data.data['temp_id']['value'] : '';
-// 	// 	super($.extend(true, {}, config, newConfig), formData.data);
-// 	// }
-//     constructor(extendConfig){
-//         super($.extend(true, {}, config, extendConfig));
-//     }
-// }
 let BaseForm = Component.extend(config)
 export default BaseForm
