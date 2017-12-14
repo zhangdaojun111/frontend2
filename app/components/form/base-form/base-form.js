e5652646b63c4f8ff167e3aaa383914e003066bf
fangqiwei <fangqiwei@wanwujieshu.net>
2017-12-14 14:13:02 +0800

    [bugfix]优化表单分页代码，修改加签弹窗的宽高

--- app/components/form/base-form/base-form.js
+++ app/components/form/base-form/base-form.js
@@ -2320,9 +2320,6 @@ let config = {
 						break;
 				}
 			}
-			$(function () {
-				$("#form-paging-tabs-control").tabs();
-			});
 		},
 
 		//改变人员信息表主岗选项
@@ -2454,9 +2451,15 @@ let config = {
 			this.actions.formStyle();
 			this.data.isInit = false;
 		},
+		//生成表单分页
+		createFormTabs(){
+			this.el.find("#form-paging-tabs-control").tabs();
+		}
 	},
 	afterRender() {
         this.actions.initForm();
+        //生成表单分页
+		this.actions.createFormTabs();
 	},
 
 	firstAfterRender(){
